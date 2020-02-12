4109e90b0540bb1c39a396547856d2d1e2a8e0de
tonokip <tonokip@gmail.com>
2020-02-12 14:06:02 -0600

    spi and encoder working with 32 bit send buffer

--- spi.v
+++ spi.v
@@ -23,56 +23,67 @@ reg [1:0] MOSIr;  always @(posedge clk) MOSIr <= {MOSIr[0], MOSI};
 wire MOSI_data = MOSIr[1];
 
 
-// we handle SPI in 32-bits format, so we need a 5 bits counter to count the bits as they come in
-reg [4:0] bitcnt;
+// we handle SPI in 8-bits format, so we need a 3 bits counter to count the bits as they come in
+reg [5:0] bitcnt;
 
 reg byte_received;  // high when a byte has been received
-reg [31:0] byte_data_received;
+reg [7:0] byte_data_received;
 
 always @(posedge clk)
 begin
   if(~SSEL_active)
-    bitcnt <= 5'b00000;
+    bitcnt <= 6'b00000;
   else
-  if(SCK_risingedge)
+  if(SCK_fallingedge)
   begin
-    bitcnt <= bitcnt + 5'b00001;
+    bitcnt <= bitcnt + 6'b00001;
 
     // implement a shift-left register (since we receive the data MSB first)
-    byte_data_received <= {byte_data_received[30:0], MOSI_data};
+    byte_data_received <= {byte_data_received[6:0], MOSI_data};
   end
 end
 
-always @(posedge clk) byte_received <= SSEL_active && SCK_risingedge && (bitcnt==3'b111);
+always @(posedge clk) byte_received <= SSEL_active && SCK_fallingedge && (bitcnt[2:0]==3'b111);
 
 // we use the LSB of the data received to control an LED
 //reg LED;
 //always @(posedge clk) if(byte_received) LED <= byte_data_received[0];
 
-////always @(posedge clk) if(byte_received) {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7} <= byte_data_received;
+///always @(posedge clk) if(byte_received) {LED7, LED6, LED5, LED4, LED3, LED2, LED1, LED0} <= byte_data_received;
 
 
 reg [31:0] byte_data_sent;
 
-//reg [7:0] cnt;
-//always @(posedge clk) if(SSEL_startmessage) cnt<=cnt+8'h1;  // count the messages
+reg [7:0] cnt;
+always @(posedge clk) if(SSEL_startmessage) cnt<=cnt+8'h1;  // count the messages
 
 always @(posedge clk)
 if(SSEL_active)
 begin
   if(SSEL_startmessage)
-    byte_data_sent <=  count; //cnt;  // first byte sent in a message is the message count
+    byte_data_sent <= count; //cnt;  // first byte sent in a message is the message count
   else
-  if(SCK_fallingedge)
+  if(SCK_risingedge)
   begin
-    if(bitcnt==5'b00000)
-      byte_data_sent <= 32'h00;  // after that, we send 0s
+    //if(bitcnt==3'b000)
+    //  byte_data_sent <= 8'h00;  // after that, we send 0s
+/*
+    if(     bitcnt == 8)
+      byte_data_sent <= count[7:0];
+    else if(bitcnt == 16)
+      byte_data_sent <= count[15:8];
+    else if(bitcnt == 24)
+      byte_data_sent <= count[23:16];
+    else if(bitcnt == 32)
+      byte_data_sent <= count[31:24];
     else
-      byte_data_sent <= {byte_data_sent[30:0], 1'b0};
+      byte_data_sent <= {byte_data_sent[6:0], 1'b0};
+*/
+    byte_data_sent <= {byte_data_sent[30:0], 1'b0};
   end
 end
 
-assign MISO = byte_data_sent[7];  // send MSB first
+assign MISO = byte_data_sent[31];  // send MSB first
 // we assume that there is only one slave on the SPI bus
 // so we don't bother with a tri-state buffer for MISO
 // otherwise we would need to tri-state MISO when SSEL is inactive
