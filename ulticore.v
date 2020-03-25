dcbca7745f05512a395af7b0854b73032cbd49fd
tonokip <tonokip@gmail.com>
2020-03-25 18:49:54 -0500

    interrupt logic prevents skipped steps at slow speeds

--- ulticore.v
+++ ulticore.v
@@ -43,13 +43,14 @@ module soc(
   output MISO,
   //input A0_P,
   //output A0_PWM
+  output POSITION_INTERRUPT,
   );
 
   
-  wire [31:0] pos_count1;
+  wire signed [31:0] pos_count1;
   
 //  reg enc1a, enc1b, enc2a, enc2b;
-  wire [31:0] count1, count2;
+  wire signed [31:0] count1, count2;
 //  reg resetn;
   wire resetn;
   reg [7:0] resetn_counter = 0;
@@ -67,15 +68,23 @@ module soc(
   
   wire [31:0] response_data;
   
-  reg [7:1] step_multiplier = 5;
-  reg [7:1] quad_multiplier = 4;
+  reg [7:1] step_multiplier;
+  reg [7:1] quad_multiplier;
   
   reg [7:0] led_pwm_value; // = 240;
+  
+  reg signed [15:0] delta_value;
+  reg signed [15:0] delta_threshold;
+  
 
   assign resetn = &resetn_counter;
 
   always @(posedge clk) begin
     if (!resetn) resetn_counter <= resetn_counter +1;
+  end
+  
+  always @(posedge clk) begin
+      delta_value <= pos_count1 - count1;
   end
   
   wire app_resetn;
@@ -176,15 +185,28 @@ module soc(
   begin
   if (!resetn)
   begin
-    //step_multiplier = 5;
-    //quad_multiplier = 4;
+    step_multiplier = 5;
+    quad_multiplier = 4;
     led_pwm_value = 240;
     LED0 = 1;
     LED1 = 1;
     LED2 = 1;
     LED3 = 1;
+    POSITION_INTERRUPT = 0;
     app_resetn = 1;
+    delta_threshold = 40;
   end
+  else begin
+
+  if( (delta_value > delta_threshold) || ( -delta_value > delta_threshold ) ) begin
+    POSITION_INTERRUPT <= 0;
+    LED0 <= 0;
+  end
+  else begin
+    POSITION_INTERRUPT <= 1;
+    LED0 <= 1;
+  end
+  
   if(byte_received)
   begin
     //led_pwm_value = byte_data_received;
@@ -245,15 +267,28 @@ module soc(
       
     // 13- LED3
     else if( byte_data_received == 13)
-      LED3 <= packet_received[0];
+      LED2 <= packet_received[0];
       
     // 14 - Read app_resetn
     else if( byte_data_received == 14)
       spi_send_data <= { 31'0, app_resetn };
       
+    // 15 - Read delta_value
+    else if( byte_data_received == 15)
+      spi_send_data <= delta_value;
+      
+    // 16 - Read delta_threshold
+    else if( byte_data_received == 16)
+      spi_send_data <= delta_threshold;
+      
+    // 17 - Write delta_threshold
+    else if( byte_data_received == 17)
+      delta_threshold <= packet_received[15:0];
+      
     // Default - Read 1234567890
     else
       spi_send_data <= 1234567890;
+  end
   end
   end
 
