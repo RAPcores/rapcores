273b62afbcaaf7cd53df62fe46f13cfcfcc9f8ab
Steve Kelly <kd2cca@gmail.com>
2020-07-16 12:59:44 -0400

    implement basic case table for stepper and visualize with LED

--- top.v
+++ top.v
@@ -2,14 +2,15 @@
 module top (
     input CLK,    // 16MHz clock
     output LED,   // User/boot LED next to power LED
-    output USBPU  // USB pull-up resistor
+    output USBPU,  // USB pull-up resistor
+    output PIN_24, // Phase A
+    output PIN_23, // Phase A
+    output PIN_22, // Phase B
+    output PIN_21, // Phase B
 );
+
     // drive USB pull-up resistor to '0' to disable USB
     assign USBPU = 0;
-
-    ////////
-    // make a simple blink circuit
-    ////////
 
     // keep track of time and location in blink_pattern
     reg [25:0] blink_counter;
@@ -20,6 +21,33 @@ module top (
     // increment the blink_counter every clock
     always @(posedge CLK) begin
         blink_counter <= blink_counter + 1;
+
+        case (blink_counter[25:21]%4)
+            0: begin // 1010
+                PIN_24 <= 1;
+                PIN_23 <= 0;
+                PIN_22 <= 1;
+                PIN_21 <= 0;
+            end
+            1:  begin // 0110
+                PIN_24 <= 0;
+                PIN_23 <= 1;
+                PIN_22 <= 1;
+                PIN_21 <= 0;
+            end
+            2:  begin //0101
+                PIN_24 <= 0;
+                PIN_23 <= 1;
+                PIN_22 <= 0;
+                PIN_21 <= 1;
+            end
+            3:  begin //1001
+                PIN_24 <= 1;
+                PIN_23 <= 0;
+                PIN_22 <= 0;
+                PIN_21 <= 1;
+            end
+        endcase
     end
 
     // light up the LED according to the pattern
