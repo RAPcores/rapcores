1720cc8302c269b7d7c09612a83f631d6e9ead75
Steve Kelly <kd2cca@gmail.com>
2020-07-16 16:12:54 -0400

    start breaking stepper into a module

--- top.v
+++ top.v
@@ -1,12 +1,11 @@
-// look in pins.pcf for all the pin names on the TinyFPGA BX board
-module top (
+module stepper (
     input CLK,    // 16MHz clock
     output LED,   // User/boot LED next to power LED
     output USBPU,  // USB pull-up resistor
-    output PIN_24, // Phase A
-    output PIN_23, // Phase A
-    output PIN_22, // Phase B
-    output PIN_21, // Phase B
+    output phase_a1, // Phase A
+    output phase_a2, // Phase A
+    output phase_b1, // Phase B
+    output phase_b2 // Phase B
 );
 
     // drive USB pull-up resistor to '0' to disable USB
@@ -14,6 +13,7 @@ module top (
 
     // keep track of time and location in blink_pattern
     reg [25:0] blink_counter;
+    reg phase_a1, phase_a2, phase_b1, phase_b2;
 
     // pattern that will be flashed over the LED over time
     wire [31:0] blink_pattern = 32'b101010001110111011100010101;
@@ -24,32 +24,52 @@ module top (
 
         case (blink_counter[25:21]%4)
             0: begin // 1010
-                PIN_24 <= 1;
-                PIN_23 <= 0;
-                PIN_22 <= 1;
-                PIN_21 <= 0;
+                phase_a1 = 1;
+                phase_a2 = 0;
+                phase_b1 = 1;
+                phase_b2 = 0;
             end
             1:  begin // 0110
-                PIN_24 <= 0;
-                PIN_23 <= 1;
-                PIN_22 <= 1;
-                PIN_21 <= 0;
+                phase_a1 <= 0;
+                phase_a2 <= 1;
+                phase_b1 <= 1;
+                phase_b2 <= 0;
             end
             2:  begin //0101
-                PIN_24 <= 0;
-                PIN_23 <= 1;
-                PIN_22 <= 0;
-                PIN_21 <= 1;
+                phase_a1 <= 0;
+                phase_a2 <= 1;
+                phase_b1 <= 0;
+                phase_b2 <= 1;
             end
             3:  begin //1001
-                PIN_24 <= 1;
-                PIN_23 <= 0;
-                PIN_22 <= 0;
-                PIN_21 <= 1;
+                phase_a1 <= 1;
+                phase_a2 <= 0;
+                phase_b1 <= 0;
+                phase_b2 <= 1;
             end
         endcase
     end
 
     // light up the LED according to the pattern
     assign LED = blink_pattern[blink_counter[25:21]];
+endmodule
+
+module top (
+    input CLK,    // 16MHz clock
+    output LED,   // User/boot LED next to power LED
+    output USBPU,  // USB pull-up resistor
+    output PIN_24, // Phase A
+    output PIN_23, // Phase A
+    output PIN_22, // Phase B
+    output PIN_21 // Phase B
+);
+
+    stepper s0 (.CLK (CLK),
+                .LED (LED),
+                .USBPU (USBPU),
+                .phase_a1 (PIN_24),
+                .phase_a2 (PIN_23),
+                .phase_b1 (PIN_22),
+                .phase_b2 (PIN_21));
+
 endmodule
