2d4aa1708546d14360f17fafeae85757e61044d1
Steve Kelly <kd2cca@gmail.com>
2020-07-17 10:46:05 -0400

    half stepping test

--- top.v
+++ top.v
@@ -12,6 +12,9 @@ module stepper (
     reg [25:0] blink_counter;
     wire phase_a1, phase_a2, phase_b1, phase_b2; //, pwm_a, pwm_b, stby;
 
+    reg [2:0] microsteps;
+    assign microsteps = 1;
+
     reg [31:0] phase_ct;
     assign pwm_a = 1; // phase a pwm TODO: microstep
     assign pwm_b = 1; // phase b pwm
@@ -19,39 +22,105 @@ module stepper (
     // increment the blink_counter every clock
     always @(posedge CLK) begin
         blink_counter <= blink_counter + 1;
-        if (blink_counter >= 100000) begin
+        if (blink_counter >= 100000/microsteps) begin
             blink_counter <= 0;
             phase_ct <= phase_ct + 1;
 
-            case (phase_ct%4)
-                0: begin // 1010
-                    phase_a1 <= 1'b1;
-                    phase_a2 <= 1'b0;
-                    phase_b1 <= 1'b1;
-                    phase_b2 <= 1'b0;
-                end
-                1:  begin // 0110
-                    phase_a1 <= 1'b0;
-                    phase_a2 <= 1'b1;
-                    phase_b1 <= 1'b1;
-                    phase_b2 <= 1'b0;
-                end
-                2:  begin //0101
-                    phase_a1 <= 1'b0;
-                    phase_a2 <= 1'b1;
-                    phase_b1 <= 1'b0;
-                    phase_b2 <= 1'b1;
-                end
-                3:  begin //1001
-                    phase_a1 <= 1'b1;
-                    phase_a2 <= 1'b0;
-                    phase_b1 <= 1'b0;
-                    phase_b2 <= 1'b1;
-                end
-            endcase
+            if (microsteps == 0 || microsteps == 1) begin
+                // TODO: Test sync vs async (on scope)
+                // TODO: Function?
+                // 1010
+                // 0110
+                // 0101
+                // 1001
+                case (phase_ct%4)
+                    0: begin // 1010
+                        phase_a1 <= 1'b1;
+                        phase_a2 <= 1'b0;
+                        phase_b1 <= 1'b1;
+                        phase_b2 <= 1'b0;
+                    end
+                    1:  begin // 0110
+                        phase_a1 <= 1'b0;
+                        phase_a2 <= 1'b1;
+                        phase_b1 <= 1'b1;
+                        phase_b2 <= 1'b0;
+                    end
+                    2:  begin //0101
+                        phase_a1 <= 1'b0;
+                        phase_a2 <= 1'b1;
+                        phase_b1 <= 1'b0;
+                        phase_b2 <= 1'b1;
+                    end
+                    3:  begin //1001
+                        phase_a1 <= 1'b1;
+                        phase_a2 <= 1'b0;
+                        phase_b1 <= 1'b0;
+                        phase_b2 <= 1'b1;
+                    end
+                endcase
+            end else if (microsteps == 2) begin
+                // 1010
+                // 0010
+                // 0110
+                // 0100
+                // 0101
+                // 0001
+                // 1001
+                // 1000
+                case (phase_ct%8)
+                    0: begin // 1010
+                        phase_a1 <= 1'b1;
+                        phase_a2 <= 1'b0;
+                        phase_b1 <= 1'b1;
+                        phase_b2 <= 1'b0;
+                    end
+                    1: begin // 0010
+                        phase_a1 <= 1'b0;
+                        phase_a2 <= 1'b0;
+                        phase_b1 <= 1'b1;
+                        phase_b2 <= 1'b0;
+                    end
+                    2:  begin // 0110
+                        phase_a1 <= 1'b0;
+                        phase_a2 <= 1'b1;
+                        phase_b1 <= 1'b1;
+                        phase_b2 <= 1'b0;
+                    end
+                    3:  begin // 0100
+                        phase_a1 <= 1'b0;
+                        phase_a2 <= 1'b1;
+                        phase_b1 <= 1'b0;
+                        phase_b2 <= 1'b0;
+                    end
+                    4:  begin // 0101
+                        phase_a1 <= 1'b0;
+                        phase_a2 <= 1'b1;
+                        phase_b1 <= 1'b0;
+                        phase_b2 <= 1'b1;
+                    end
+                    5:  begin // 0001
+                        phase_a1 <= 1'b0;
+                        phase_a2 <= 1'b0;
+                        phase_b1 <= 1'b0;
+                        phase_b2 <= 1'b1;
+                    end
+                    6:  begin // 1001
+                        phase_a1 <= 1'b1;
+                        phase_a2 <= 1'b0;
+                        phase_b1 <= 1'b0;
+                        phase_b2 <= 1'b1;
+                    end
+                    7:  begin // 1000
+                        phase_a1 <= 1'b1;
+                        phase_a2 <= 1'b0;
+                        phase_b1 <= 1'b0;
+                        phase_b2 <= 1'b0;
+                    end
+                endcase
+            end
         end
     end
-
 endmodule
 
 module top (
