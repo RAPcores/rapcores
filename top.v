76cccaa703b671a303fec8b659feceb6d69938e8
Steve Kelly <kd2cca@gmail.com>
2020-07-27 12:13:43 -0400

    init spi, untested

--- top.v
+++ top.v
@@ -1,4 +1,5 @@
 `include "stepper.v"
+`include "spi.v"
 
 module top (
     input CLK,    // 16MHz clock
@@ -18,6 +19,12 @@ module top (
     // drive USB pull-up resistor to '0' to disable USB
     assign USBPU = 0;
 
+    wire byte_received;  // high when a byte has been received
+    wire [7:0] byte_data_received;
+    wire [31:0] packet_received;
+
+    reg [31:0] spi_send_data;
+
     stepper s0 (.CLK (CLK),
                 .phase_a1 (PIN_8),
                 .phase_a2 (PIN_9),
@@ -25,11 +32,35 @@ module top (
                 .phase_b2 (PIN_12),
                 .pwm_a (PIN_7),
                 .pwm_b (PIN_13));
-    stepper s1 (.CLK (CLK),
-                 .phase_a1 (PIN_24),
-                 .phase_a2 (PIN_23),
-                 .phase_b1 (PIN_22),
-                 .phase_b2 (PIN_21),
-                 .pwm_a (),
-                 .pwm_b ());
+    // LED Stepper
+    // stepper s1 (.CLK (CLK),
+    //              .phase_a1 (PIN_24),
+    //              .phase_a2 (PIN_23),
+    //              .phase_b1 (PIN_22),
+    //              .phase_b2 (PIN_21),
+    //              .pwm_a (),
+    //              .pwm_b ());
+
+    spi spi0(.clk(CLK),
+            .SCK(PIN_14),
+            .SSEL(PIN_15),
+            .MOSI(PIN_16),
+            .MISO(PIN_17),
+            .send_data(spi_send_data),
+            .byte_received(byte_received),
+            .rx_data( {byte_data_received, packet_received} ) );
+
+
+    always @(posedge byte_received) begin
+        case (byte_data_received)
+            //led_pwm_value = byte_data_received;
+
+            // 1 - Read Step Position
+            1: PIN_24 <= 1;
+
+            // 2 - Read Quadrature
+            2: PIN_24 <= 0;
+        endcase
+    end
+
 endmodule
