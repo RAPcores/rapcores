98e0976cdf61bb33b425ac16200f29e58cbe3184
Steve Kelly <kd2cca@gmail.com>
2020-08-05 19:49:43 -0400

    get word handling workding

--- spi.v
+++ spi.v
@@ -30,13 +30,8 @@ wire SSEL_endmessage = (SSELr[2:1]==2'b01);  // message stops at rising edge
 reg [1:0] MOSIr;  always @(posedge clk) MOSIr <= {MOSIr[0], MOSI};
 wire MOSI_data = MOSIr[1];
 
-
 // we handle SPI in 8-bits format, so we need a 3 bits counter to count the bits as they come in
 reg [2:0] bitcnt;
-
-reg byte_received;  // high when a byte has been received
-
-reg [7:0] last_byte_received;
 
 reg [rxwidth-1:0] rx_data;
 
@@ -55,7 +50,6 @@ begin
 end
 
 always @(posedge clk) byte_received <= SSEL_active && SCK_risingedge && (bitcnt[2:0]==3'b111);
-always @(posedge clk) if(byte_received) last_byte_received <= byte_data_received;
 
 reg [7:0] byte_data_sent;
 
@@ -68,12 +62,7 @@ begin
   if(SSEL_startmessage)
     byte_data_sent <= send_data;
   else if(SCK_fallingedge)
-  begin
-    if(bitcnt==3'b000)
-      byte_data_sent <= 8'h00;  // after that, we send 0s
-    else
-      byte_data_sent <= {byte_data_sent[6:0], 1'b0};
-  end
+    byte_data_sent <= {byte_data_sent[6:0], 1'b0};
 end
 
 
@@ -81,5 +70,36 @@ assign MISO = byte_data_sent[7];  // send MSB first
 // we assume that there is only one slave on the SPI bus
 // so we don't bother with a tri-state buffer for MISO
 // otherwise we would need to tri-state MISO when SSEL is inactive
+
+endmodule
+
+
+module spi_packet(
+    input clk,
+  output [7:0] send_data,
+  output [31:0] word_send_data,
+  input byte_received,
+  output reg word_received,
+  output [7:0] byte_data_received,
+  output [31:0] word_data_received,
+  output LED1,
+  output LED2,
+  output LED3);
+
+reg [2:0] byte_count;
+
+always @(posedge byte_received) begin
+    byte_count <= byte_count + 3'b001;
+    word_data_received <= {byte_received[7:0], word_data_received[31:7]};
+    LED1 <= byte_count[0];
+    LED2 <= byte_count[1];
+    if (byte_count[2:0]==3'b100) begin
+        LED3 <= ~LED3;
+        byte_count <= 3'b000;
+
+    end
+end
+
+assign word_received = (byte_count[2:0]==3'b100);
 
 endmodule
--- top.v
+++ top.v
@@ -18,17 +18,44 @@ module top (
     output PIN_22,
     output PIN_21,
     output PIN_20,
+    output PIN_18,
+    output PIN_19,
     output PIN_7,
-    output PIN_13,);
+    output PIN_13);
 
     // drive USB pull-up resistor to '0' to disable USB
     assign USBPU = 0;
 
+    // SPI Initialization
+    // The standard unit of transfer is 8 bits, MSB
     wire byte_received;  // high when a byte has been received
     wire [7:0] byte_data_received;
-    wire [7:0] packet_received;
-
     reg [7:0] spi_send_data;
+    spi spi0 (.clk(CLK),
+        .SCK(PIN_1),
+        .SSEL(PIN_2),
+        .MOSI(PIN_3),
+        .MISO(PIN_4),
+        .send_data(spi_send_data),
+        .byte_received(byte_received),
+        .byte_data_received(byte_data_received) );
+
+    // Word handler
+    // The system operates on 32 bit little endian words
+    reg [31:0] word_send_data;
+    wire [31:0] word_data_received;
+    reg word_received;
+    spi_packet word_proc (
+                .clk(CLK),
+                .send_data(spi_send_data),
+                .word_send_data(word_send_data),
+                .byte_received(byte_received),
+                .word_received(word_received),
+                .byte_data_received(byte_data_received),
+                .word_data_received(word_data_received),
+                .LED1(PIN_18),
+                .LED2(PIN_19),
+                .LED3(PIN_20));
 
     stepper s0 (.CLK (CLK),
                 .phase_a1 (PIN_8),
@@ -38,22 +65,12 @@ module top (
                 .pwm_a (PIN_7),
                 .pwm_b (PIN_13));
 
-    spi spi0(.clk(CLK),
-            .SCK(PIN_1),
-            .SSEL(PIN_2),
-            .MOSI(PIN_3),
-            .MISO(PIN_4),
-            .send_data(spi_send_data),
-            .byte_received(byte_received),
-            .byte_data_received(byte_data_received) );
-
-
-    always @(posedge byte_received) begin
-        PIN_20 <= ~PIN_20;
-        PIN_24 <= byte_data_received[0];
-        PIN_23 <= byte_data_received[1];
-        PIN_22 <= byte_data_received[2];
-        spi_send_data[7:0] = byte_data_received[7:0];
+    always @(posedge word_received) begin
+        PIN_21 <= ~PIN_21;
+        PIN_24 <= word_data_received[0];
+        PIN_23 <= word_data_received[1];
+        PIN_22 <= word_data_received[2];
+        spi_send_data[7:0] = word_data_received[7:0];
     end
 
 endmodule
