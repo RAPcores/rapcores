7b71403c40a90c79d71eef4561f2cec7816de03d
Steve Kelly <kd2cca@gmail.com>
2020-08-05 21:42:02 -0400

    apply code format

--- spi.v
+++ spi.v
@@ -1,102 +1,96 @@
 module spi (
-  input clk,
-  input SCK,
-  input SSEL,
-  input MOSI,
-  output reg MISO,
-  input [7:0] send_data,
-  output byte_received,
-  output [7:0] byte_data_received);
-parameter txwidth = 8;
-parameter rxwidth = 8;
+    input            clk,
+    input            SCK,
+    input            SSEL,
+    input            MOSI,
+    output reg       MISO,
+    input      [7:0] send_data,
+    output           byte_received,
+    output     [7:0] byte_data_received
+);
+  parameter txwidth = 8;
+  parameter rxwidth = 8;
 
-// sync SCK to the FPGA clock using a 3-bits shift register
-reg [2:0] SCKr;
-always @(posedge clk) SCKr <= {SCKr[1:0], SCK};
+  // sync SCK to the FPGA clock using a 3-bits shift register
+  reg [2:0] SCKr;
+  always @(posedge clk) SCKr <= {SCKr[1:0], SCK};
 
-wire SCK_risingedge = (SCKr[2:1]==2'b01);  // now we can detect SCK rising edges
-wire SCK_fallingedge = (SCKr[2:1]==2'b10);  // and falling edges
+  wire SCK_risingedge = (SCKr[2:1] == 2'b01);  // now we can detect SCK rising edges
+  wire SCK_fallingedge = (SCKr[2:1] == 2'b10);  // and falling edges
 
-// same thing for SSEL
-reg [2:0] SSELr;
-always @(posedge clk) SSELr <= {SSELr[1:0], SSEL};
+  // same thing for SSEL
+  reg [2:0] SSELr;
+  always @(posedge clk) SSELr <= {SSELr[1:0], SSEL};
 
-wire SSEL_active = ~SSELr[1];  // SSEL is active low
-wire SSEL_startmessage = (SSELr[2:1]==2'b10);  // message starts at falling edge
-wire SSEL_endmessage = (SSELr[2:1]==2'b01);  // message stops at rising edge
+  wire SSEL_active = ~SSELr[1];  // SSEL is active low
+  wire SSEL_startmessage = (SSELr[2:1] == 2'b10);  // message starts at falling edge
+  wire SSEL_endmessage = (SSELr[2:1] == 2'b01);  // message stops at rising edge
 
-// and for MOSI
-reg [1:0] MOSIr;  always @(posedge clk) MOSIr <= {MOSIr[0], MOSI};
-wire MOSI_data = MOSIr[1];
+  // and for MOSI
+  reg [1:0] MOSIr;
+  always @(posedge clk) MOSIr <= {MOSIr[0], MOSI};
+  wire MOSI_data = MOSIr[1];
 
-// we handle SPI in 8-bits format, so we need a 3 bits counter to count the bits as they come in
-reg [2:0] bitcnt;
+  // we handle SPI in 8-bits format, so we need a 3 bits counter to count the bits as they come in
+  reg [2:0] bitcnt;
 
-reg [rxwidth-1:0] rx_data;
+  reg [rxwidth-1:0] rx_data;
 
-always @(posedge clk)
-begin
-  if(~SSEL_active)
-    bitcnt <= 3'b000;
-  else
-  if(SCK_risingedge)
-  begin
-    bitcnt <= bitcnt + 3'b001;
+  always @(posedge clk) begin
+    if (~SSEL_active) bitcnt <= 3'b000;
+    else if (SCK_risingedge) begin
+      bitcnt <= bitcnt + 3'b001;
 
-    // implement a shift-left register (since we receive the data MSB first)
-    byte_data_received <= {byte_data_received[rxwidth-2:0], MOSI_data};
+      // implement a shift-left register (since we receive the data MSB first)
+      byte_data_received <= {byte_data_received[rxwidth - 2:0], MOSI_data};
+    end
   end
-end
 
-always @(posedge clk) byte_received <= SSEL_active && SCK_risingedge && (bitcnt[2:0]==3'b111);
+  always @(posedge clk) byte_received <= SSEL_active && SCK_risingedge && (bitcnt[2:0] == 3'b111);
 
-reg [7:0] byte_data_sent;
+  reg [7:0] byte_data_sent;
 
-reg [7:0] cnt;
-always @(posedge clk) if(SSEL_startmessage) cnt<=cnt+8'h1;  // count the messages
+  reg [7:0] cnt;
+  always @(posedge clk) if (SSEL_startmessage) cnt <= cnt + 8'h1;  // count the messages
 
-always @(posedge clk)
-if(SSEL_active)
-begin
-  if(SSEL_startmessage)
-    byte_data_sent <= send_data;
-  else if(SCK_fallingedge)
-    byte_data_sent <= {byte_data_sent[6:0], 1'b0};
-end
+  always @(posedge clk)
+    if (SSEL_active) begin
+      if (SSEL_startmessage) byte_data_sent <= send_data;
+      else if (SCK_fallingedge) byte_data_sent <= {byte_data_sent[6:0], 1'b0};
+    end
 
 
-assign MISO = byte_data_sent[7];  // send MSB first
-// we assume that there is only one slave on the SPI bus
-// so we don't bother with a tri-state buffer for MISO
-// otherwise we would need to tri-state MISO when SSEL is inactive
+  assign MISO = byte_data_sent[7];  // send MSB first
+  // we assume that there is only one slave on the SPI bus
+  // so we don't bother with a tri-state buffer for MISO
+  // otherwise we would need to tri-state MISO when SSEL is inactive
 
 endmodule
 
 
-module spi_packet(
-    input clk,
-  output [7:0] send_data,
-  output [31:0] word_send_data,
-  input byte_received,
-  output word_received,
-  output [7:0] byte_data_received,
-  output [31:0] word_data_received,
-  output LED1,
-  output LED2,
-  output LED3);
+module spi_packet (
+    input         clk,
+    output [ 7:0] send_data,
+    output [31:0] word_send_data,
+    input         byte_received,
+    output        word_received,
+    output [ 7:0] byte_data_received,
+    output [31:0] word_data_received,
+    output        LED1,
+    output        LED2,
+    output        LED3
+);
 
-reg [2:0] byte_count;
+  reg [2:0] byte_count;
 
-always @(posedge byte_received) begin
-    if (byte_count[2:0]==3'b100)
-        byte_count = 3'b001;
-    else
-        byte_count = byte_count + 3'b001;
+  always @(posedge byte_received) begin
+    if (byte_count[2:0] == 3'b100) byte_count = 3'b001;
+    else byte_count = byte_count + 3'b001;
     word_data_received = {byte_data_received[7:0], word_data_received[31:7]};
     LED1 = byte_count[0];
     LED2 = byte_count[1];
-end
+  end
 
-always @(posedge clk) word_received <= (byte_count[2:0]==3'b100);
+  always @(posedge clk) word_received <= (byte_count[2:0] == 3'b100);
 
 endmodule
--- top.v
+++ top.v
@@ -2,17 +2,17 @@
 `include "spi.v"
 
 module top (
-    input CLK,    // 16MHz clock
-    output LED,   // User/boot LED next to power LED
+    input  CLK,  // 16MHz clock
+    output LED,  // User/boot LED next to power LED
     output USBPU,  // USB pull-up resistor
-    input PIN_1,
-    input PIN_2,
-    input PIN_3,
+    input  PIN_1,
+    input  PIN_2,
+    input  PIN_3,
     output PIN_4,
-    output PIN_8, // Phase A
-    output PIN_9, // Phase A
-    output PIN_11, // Phase B
-    output PIN_12, // Phase B
+    output PIN_8,  // Phase A
+    output PIN_9,  // Phase A
+    output PIN_11,  // Phase B
+    output PIN_12,  // Phase B
     output PIN_24,
     output PIN_23,
     output PIN_22,
@@ -21,17 +21,18 @@ module top (
     output PIN_18,
     output PIN_19,
     output PIN_7,
-    output PIN_13);
+    output PIN_13
+);
 
-    // drive USB pull-up resistor to '0' to disable USB
-    assign USBPU = 0;
+  // drive USB pull-up resistor to '0' to disable USB
+  assign USBPU = 0;
 
-    // SPI Initialization
-    // The standard unit of transfer is 8 bits, MSB
-    wire byte_received;  // high when a byte has been received
-    wire [7:0] byte_data_received;
-    reg [7:0] spi_send_data;
-    spi spi0 (.clk(CLK),
+  // SPI Initialization
+  // The standard unit of transfer is 8 bits, MSB
+  wire byte_received;  // high when a byte has been received
+  wire [7:0] byte_data_received;
+  reg [7:0] spi_send_data;
+  spi spi0 (.clk(CLK),
         .SCK(PIN_1),
         .SSEL(PIN_2),
         .MOSI(PIN_3),
@@ -40,12 +41,13 @@ module top (
         .byte_received(byte_received),
         .byte_data_received(byte_data_received) );
 
-    // Word handler
-    // The system operates on 32 bit little endian words
-    reg [31:0] word_send_data;
-    wire [31:0] word_data_received;
-    wire word_received;
-    spi_packet word_proc (
+  // Word handler
+  // The system operates on 32 bit little endian words
+  // This should make it easier to send 32 bit chunks from the host controller
+  reg [31:0] word_send_data;
+  wire [31:0] word_data_received;
+  wire word_received;
+  spi_packet word_proc (
                 .clk(CLK),
                 .send_data(spi_send_data),
                 .word_send_data(word_send_data),
@@ -57,7 +59,7 @@ module top (
                 .LED2(PIN_19),
                 .LED3(PIN_20));
 
-    stepper s0 (.CLK (CLK),
+  stepper s0 (.CLK (CLK),
                 .phase_a1 (PIN_8),
                 .phase_a2 (PIN_9),
                 .phase_b1 (PIN_11),
@@ -65,12 +67,12 @@ module top (
                 .pwm_a (PIN_7),
                 .pwm_b (PIN_13));
 
-    always @(posedge word_received) begin
-        PIN_21 <= ~PIN_21;
-        PIN_24 <= word_data_received[0];
-        PIN_23 <= word_data_received[1];
-        PIN_22 <= word_data_received[2];
-        spi_send_data[7:0] = word_data_received[7:0];
-    end
+  always @(posedge word_received) begin
+    PIN_21 <= ~PIN_21;
+    PIN_24 <= word_data_received[0];
+    PIN_23 <= word_data_received[1];
+    PIN_22 <= word_data_received[2];
+    spi_send_data[7:0] = word_data_received[7:0];
+  end
 
 endmodule
