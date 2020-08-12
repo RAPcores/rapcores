cc031b4c83a25160efb97118649a1e17791e5dbf
Steve Kelly <kd2cca@gmail.com>
2020-08-12 11:31:39 -0400

    clear out some warnings in stepper routine

--- stepper.v
+++ stepper.v
@@ -16,12 +16,21 @@ module DualHBridge (
   assign pwm_a = 1;  // phase a pwm TODO: microstep
   assign pwm_b = 1;  // phase b pwm
 
+  reg pa1 = 1'b0;
+  reg pa2 = 1'b0;
+  reg pb1 = 1'b0;
+  reg pb2 = 1'b0;
+
+  assign phase_a1 = pa1;
+  assign phase_a2 = pa2;
+  assign phase_b1 = pb1;
+  assign phase_b2 = pb2;
+
   // increment the move_ticks every clock
   always @(posedge step) begin
     phase_ct <= phase_ct + 1;
 
     if (microsteps == 0 || microsteps == 1) begin
-      // TODO: Test sync vs async (on scope)
       // TODO: Function?
       // 1010
       // 0110
@@ -29,28 +38,28 @@ module DualHBridge (
       // 1001
       case (phase_ct % 4)
         0: begin  // 1010
-          phase_a1 <= 1'b1;
-          phase_a2 <= 1'b0;
-          phase_b1 <= 1'b1;
-          phase_b2 <= 1'b0;
+          pa1 <= 1'b1;
+          pa2 <= 1'b0;
+          pb1 <= 1'b1;
+          pb2 <= 1'b0;
         end
         1: begin  // 0110
-          phase_a1 <= 1'b0;
-          phase_a2 <= 1'b1;
-          phase_b1 <= 1'b1;
-          phase_b2 <= 1'b0;
+          pa1 <= 1'b0;
+          pa2 <= 1'b1;
+          pb1 <= 1'b1;
+          pb2 <= 1'b0;
         end
         2: begin  //0101
-          phase_a1 <= 1'b0;
-          phase_a2 <= 1'b1;
-          phase_b1 <= 1'b0;
-          phase_b2 <= 1'b1;
+          pa1 <= 1'b0;
+          pa2 <= 1'b1;
+          pb1 <= 1'b0;
+          pb2 <= 1'b1;
         end
         3: begin  //1001
-          phase_a1 <= 1'b1;
-          phase_a2 <= 1'b0;
-          phase_b1 <= 1'b0;
-          phase_b2 <= 1'b1;
+          pa1 <= 1'b1;
+          pa2 <= 1'b0;
+          pb1 <= 1'b0;
+          pb2 <= 1'b1;
         end
       endcase
     end else if (microsteps == 2) begin
@@ -64,52 +73,52 @@ module DualHBridge (
       // 1000
       case (phase_ct % 8)
         0: begin  // 1010
-          phase_a1 <= 1'b1;
-          phase_a2 <= 1'b0;
-          phase_b1 <= 1'b1;
-          phase_b2 <= 1'b0;
+          pa1 <= 1'b1;
+          pa2 <= 1'b0;
+          pb1 <= 1'b1;
+          pb2 <= 1'b0;
         end
         1: begin  // 0010
-          phase_a1 <= 1'b0;
-          phase_a2 <= 1'b0;
-          phase_b1 <= 1'b1;
-          phase_b2 <= 1'b0;
+          pa1 <= 1'b0;
+          pa2 <= 1'b0;
+          pb1 <= 1'b1;
+          pb2 <= 1'b0;
         end
         2: begin  // 0110
-          phase_a1 <= 1'b0;
-          phase_a2 <= 1'b1;
-          phase_b1 <= 1'b1;
-          phase_b2 <= 1'b0;
+          pa1 <= 1'b0;
+          pa2 <= 1'b1;
+          pb1 <= 1'b1;
+          pb2 <= 1'b0;
         end
         3: begin  // 0100
-          phase_a1 <= 1'b0;
-          phase_a2 <= 1'b1;
-          phase_b1 <= 1'b0;
-          phase_b2 <= 1'b0;
+          pa1 <= 1'b0;
+          pa2 <= 1'b1;
+          pb1 <= 1'b0;
+          pb2 <= 1'b0;
         end
         4: begin  // 0101
-          phase_a1 <= 1'b0;
-          phase_a2 <= 1'b1;
-          phase_b1 <= 1'b0;
-          phase_b2 <= 1'b1;
+          pa1 <= 1'b0;
+          pa2 <= 1'b1;
+          pb1 <= 1'b0;
+          pb2 <= 1'b1;
         end
         5: begin  // 0001
-          phase_a1 <= 1'b0;
-          phase_a2 <= 1'b0;
-          phase_b1 <= 1'b0;
-          phase_b2 <= 1'b1;
+          pa1 <= 1'b0;
+          pa2 <= 1'b0;
+          pb1 <= 1'b0;
+          pb2 <= 1'b1;
         end
         6: begin  // 1001
-          phase_a1 <= 1'b1;
-          phase_a2 <= 1'b0;
-          phase_b1 <= 1'b0;
-          phase_b2 <= 1'b1;
+          pa1 <= 1'b1;
+          pa2 <= 1'b0;
+          pb1 <= 1'b0;
+          pb2 <= 1'b1;
         end
         7: begin  // 1000
-          phase_a1 <= 1'b1;
-          phase_a2 <= 1'b0;
-          phase_b1 <= 1'b0;
-          phase_b2 <= 1'b0;
+          pa1 <= 1'b1;
+          pa2 <= 1'b0;
+          pb1 <= 1'b0;
+          pb2 <= 1'b0;
         end
       endcase
     end
