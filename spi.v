e8f3abd60ca607e3aea34a6a36b8a65792537fd2
Steve Kelly <kd2cca@gmail.com>
2020-08-17 10:54:06 -0400

    finish swap out for MIT licensed SPI implmentation, should address backfeed mailboxing issue

--- spi.v
+++ spi.v
@@ -1,74 +1,233 @@
 `default_nettype none
 
-// TODO license unknown, replace with MIT version in soft_spi.v
-module SPI (
-    input            clk,
-    input            SCK,
-    input            SSEL,
-    input            MOSI,
-    output reg       MISO,
-    input      [7:0] send_data,
-    output           byte_received,
-    output reg    [7:0] byte_data_received
-);
-  parameter txwidth = 8;
-  parameter rxwidth = 8;
+// Derived from: https://github.com/nandland/spi-slave
+// MIT License
 
-  // sync SCK to the FPGA clock using a 3-bits shift register
-  reg [2:0] SCKr;
-  always @(posedge clk) SCKr <= {SCKr[1:0], SCK};
+// Copyright (c) 2019 russell-merrick
 
-  wire SCK_risingedge = (SCKr[2:1] == 2'b01);  // now we can detect SCK rising edges
-  wire SCK_fallingedge = (SCKr[2:1] == 2'b10);  // and falling edges
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 
-  // same thing for SSEL
-  reg [2:0] SSELr;
-  always @(posedge clk) SSELr <= {SSELr[1:0], SSEL};
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 
-  wire SSEL_active = ~SSELr[1];  // SSEL is active low
-  wire SSEL_startmessage = (SSELr[2:1] == 2'b10);  // message starts at falling edge
-  wire SSEL_endmessage = (SSELr[2:1] == 2'b01);  // message stops at rising edge
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-  // and for MOSI
-  reg [1:0] MOSIr;
-  always @(posedge clk) MOSIr <= {MOSIr[0], MOSI};
-  wire MOSI_data = MOSIr[1];
 
-  // we handle SPI in 8-bits format, so we need a 3 bits counter to count the bits as they come in
-  reg [2:0] bitcnt;
+///////////////////////////////////////////////////////////////////////////////
+// Description: SPI (Serial Peripheral Interface) Slave
+//              Creates slave based on input configuration.
+//              Receives a byte one bit at a time on MOSI
+//              Will also push out byte data one bit at a time on MISO.
+//              Any data on input byte will be shipped out on MISO.
+//              Supports multiple bytes per transaction when CS_n is kept
+//              low during the transaction.
+//
+// Note:        i_Clk must be at least 4x faster than i_SPI_Clk
+//              MISO is tri-stated when not communicating.  Allows for multiple
+//              SPI Slaves on the same interface.
+//
+// Parameters:  SPI_MODE, can be 0, 1, 2, or 3.  See above.
+//              Can be configured in one of 4 modes:
+//              Mode | Clock Polarity (CPOL/CKP) | Clock Phase (CPHA)
+//               0   |             0             |        0
+//               1   |             0             |        1
+//               2   |             1             |        0
+//               3   |             1             |        1
+//              More info: https://en.wikipedia.org/wiki/Serial_Peripheral_Interface_Bus#Mode_numbers
+///////////////////////////////////////////////////////////////////////////////
 
-  reg [rxwidth-1:0] rx_data;
+// n.b
+// The new language for SPI pin labeling recommends the use of SDO/SDI (Serial Data Out/In)
+// for single-role hardware, and COPI/CIPO for “Controller Out, Peripheral In” and
+// “Controller In, Peripheral Out” for devices that can be either the controller or
+// the peripheral. The change also updates the “SS” (Slave Select) pin to use “CS” (Chip Select).
 
-  always @(posedge clk) begin
-    if (~SSEL_active) bitcnt <= 3'b000;
-    else if (SCK_risingedge) begin
-      bitcnt <= bitcnt + 3'b001;
+module SPI_Slave
+  #(parameter SPI_MODE = 0)
+  (
+   // Control/Data Signals,
+   input            i_Rst_L,    // FPGA Reset, active low
+   input            i_Clk,      // FPGA Clock
+   output reg       o_RX_DV,    // Data Valid pulse (1 clock cycle)
+   output reg [7:0] o_RX_Byte,  // Byte received on MOSI
+   input            i_TX_DV,    // Data Valid pulse to register i_TX_Byte
+   input  [7:0]     i_TX_Byte,  // Byte to serialize to MISO.
 
-      // implement a shift-left register (since we receive the data MSB first)
-      byte_data_received <= {byte_data_received[rxwidth - 2:0], MOSI_data};
+   // SPI Interface
+   input      i_SPI_Clk,
+   output reg o_SPI_MISO,
+   input      i_SPI_MOSI,
+   input      i_SPI_CS_n // active low
+   );
+
+
+  // SPI Interface (All Runs at SPI Clock Domain)
+  wire w_CPOL;     // Clock polarity
+  wire w_CPHA;     // Clock phase
+  wire w_SPI_Clk;  // Inverted/non-inverted depending on settings
+  wire w_SPI_MISO_Mux;
+
+  reg [2:0] r_RX_Bit_Count;
+  reg [2:0] r_TX_Bit_Count;
+  reg [7:0] r_Temp_RX_Byte;
+  reg [7:0] r_RX_Byte;
+  reg r_RX_Done, r2_RX_Done, r3_RX_Done;
+  reg [7:0] r_TX_Byte;
+  reg r_SPI_MISO_Bit, r_Preload_MISO;
+
+  // CPOL: Clock Polarity
+  // CPOL=0 means clock idles at 0, leading edge is rising edge.
+  // CPOL=1 means clock idles at 1, leading edge is falling edge.
+  assign w_CPOL  = (SPI_MODE == 2) | (SPI_MODE == 3);
+
+  // CPHA: Clock Phase
+  // CPHA=0 means the "out" side changes the data on trailing edge of clock
+  //              the "in" side captures data on leading edge of clock
+  // CPHA=1 means the "out" side changes the data on leading edge of clock
+  //              the "in" side captures data on the trailing edge of clock
+  assign w_CPHA  = (SPI_MODE == 1) | (SPI_MODE == 3);
+
+  assign w_SPI_Clk = w_CPHA ? ~i_SPI_Clk : i_SPI_Clk;
+
+
+
+  // Purpose: Recover SPI Byte in SPI Clock Domain
+  // Samples line on correct edge of SPI Clock
+  always @(posedge w_SPI_Clk or posedge i_SPI_CS_n)
+  begin
+    if (i_SPI_CS_n)
+    begin
+      r_RX_Bit_Count <= 0;
+      r_RX_Done      <= 1'b0;
+    end
+    else
+    begin
+      r_RX_Bit_Count <= r_RX_Bit_Count + 1;
+
+      // Receive in LSB, shift up to MSB
+      r_Temp_RX_Byte <= {r_Temp_RX_Byte[6:0], i_SPI_MOSI};
+
+      if (r_RX_Bit_Count == 3'b111)
+      begin
+        r_RX_Done <= 1'b1;
+        r_RX_Byte <= {r_Temp_RX_Byte[6:0], i_SPI_MOSI};
+      end
+      else if (r_RX_Bit_Count == 3'b010)
+      begin
+        r_RX_Done <= 1'b0;
+      end
+
+    end // else: !if(i_SPI_CS_n)
+  end // always @ (posedge w_SPI_Clk or posedge i_SPI_CS_n)
+
+
+
+  // Purpose: Cross from SPI Clock Domain to main FPGA clock domain
+  // Assert o_RX_DV for 1 clock cycle when o_RX_Byte has valid data.
+  always @(posedge i_Clk or negedge i_Rst_L)
+  begin
+    if (~i_Rst_L)
+    begin
+      r2_RX_Done <= 1'b0;
+      r3_RX_Done <= 1'b0;
+      o_RX_DV    <= 1'b0;
+      o_RX_Byte  <= 8'h00;
+    end
+    else
+    begin
+      // Here is where clock domains are crossed.
+      // This will require timing constraint created, can set up long path.
+      r2_RX_Done <= r_RX_Done;
+
+      r3_RX_Done <= r2_RX_Done;
+
+      if (r3_RX_Done == 1'b0 && r2_RX_Done == 1'b1) // rising edge
+      begin
+        o_RX_DV   <= 1'b1;  // Pulse Data Valid 1 clock cycle
+        o_RX_Byte <= r_RX_Byte;
+      end
+      else
+      begin
+        o_RX_DV <= 1'b0;
+      end
+    end // else: !if(~i_Rst_L)
+  end // always @ (posedge i_Bus_Clk)
+
+
+  // Control preload signal.  Should be 1 when CS is high, but as soon as
+  // first clock edge is seen it goes low.
+  always @(posedge w_SPI_Clk or posedge i_SPI_CS_n)
+  begin
+    if (i_SPI_CS_n)
+    begin
+      r_Preload_MISO <= 1'b1;
+    end
+    else
+    begin
+      r_Preload_MISO <= 1'b0;
     end
   end
 
-  always @(posedge clk) byte_received <= SSEL_active && SCK_risingedge && (bitcnt[2:0] == 3'b111);
 
-  reg [7:0] byte_data_sent;
-
-  reg [7:0] cnt;
-  always @(posedge clk) if (SSEL_startmessage) cnt <= cnt + 8'h1;  // count the messages
-
-  always @(posedge clk)
-    if (SSEL_active) begin
-      if (SSEL_startmessage) byte_data_sent <= send_data;
-      else if (SCK_fallingedge) byte_data_sent <= {byte_data_sent[6:0], 1'b0};
+  // Purpose: Transmits 1 SPI Byte whenever SPI clock is toggling
+  // Will transmit read data back to SW over MISO line.
+  // Want to put data on the line immediately when CS goes low.
+  always @(posedge w_SPI_Clk or posedge i_SPI_CS_n)
+  begin
+    if (i_SPI_CS_n)
+    begin
+      r_TX_Bit_Count <= 3'b111;  // Send MSb first
+      r_SPI_MISO_Bit <= r_TX_Byte[3'b111];  // Reset to MSb
     end
+    else
+    begin
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;
+
+      // Here is where data crosses clock domains from i_Clk to w_SPI_Clk
+      // Can set up a timing constraint with wide margin for data path.
+      r_SPI_MISO_Bit <= r_TX_Byte[r_TX_Bit_Count];
+
+    end // else: !if(i_SPI_CS_n)
+  end // always @ (negedge w_SPI_Clk or posedge i_SPI_CS_n_SW)
 
 
-  assign MISO = byte_data_sent[7];  // send MSB first
-  // we assume that there is only one slave on the SPI bus
-  // so we don't bother with a tri-state buffer for MISO
-  // otherwise we would need to tri-state MISO when SSEL is inactive
+  // Purpose: Register TX Byte when DV pulse comes.  Keeps registed byte in
+  // this module to get serialized and sent back to master.
+  always @(posedge i_Clk or negedge i_Rst_L)
+  begin
+    if (~i_Rst_L)
+    begin
+      r_TX_Byte <= 8'h00;
+    end
+    else
+    begin
+      if (i_TX_DV)
+      begin
+        r_TX_Byte <= i_TX_Byte;
+      end
+    end // else: !if(~i_Rst_L)
+  end // always @ (posedge i_Clk or negedge i_Rst_L)
 
-endmodule
+  // Preload MISO with top bit of send data when preload selector is high.
+  // Otherwise just send the normal MISO data
+  assign w_SPI_MISO_Mux = r_Preload_MISO ? r_TX_Byte[3'b111] : r_SPI_MISO_Bit;
+
+  // Tri-statae MISO when CS is high.  Allows for multiple slaves to talk.
+  assign o_SPI_MISO = i_SPI_CS_n ? 1'bZ : w_SPI_MISO_Mux;
+
+endmodule // SPI_Slave
+
 
 
 // 32 bit word SPI wrapper for Little endian 8 bit transfers
@@ -89,15 +248,20 @@ module SPIWord (
   wire byte_received;  // high when a byte has been received
   reg [7:0] byte_data_received;
   reg [7:0] send_data;
-  SPI spi0 (.clk(clk),
-            .SCK(SCK),
-            .SSEL(SSEL),
-            .MOSI(MOSI),
-            .MISO(MISO),
-            .send_data(send_data),
-            .byte_received(byte_received),
-            .byte_data_received(byte_data_received) );
-
+  wire reset;
+  assign reset = 1;
+  reg SPI_TX_DV;
+  SPI_Slave spi0 (
+            .i_Rst_L(reset),
+            .i_Clk(clk),
+            .o_RX_DV(byte_received),
+            .o_RX_Byte(byte_data_received),
+            .i_TX_DV(SPI_TX_DV),
+            .i_TX_Byte(send_data),
+            .i_SPI_Clk(SCK),
+            .o_SPI_MISO(MISO),
+            .i_SPI_MOSI(MOSI),
+            .i_SPI_CS_n(SSEL));
 
   reg [2:0] byte_count = 0;
 
@@ -105,13 +269,14 @@ module SPIWord (
   always @(posedge byte_received) begin
     byte_count = (byte_count == 4) ? 1 : byte_count + 1;
     word_data_received = {byte_data_received[7:0], word_data_received[31:8]};
+    send_data = 8'h00;
   end
 
   always @(posedge clk) begin
-    word_received <= (byte_count[2:0] == 3'b100);
-    if (byte_count == 4) send_data[7:0] <= word_send_data[7:0];
-    if (byte_count == 1) send_data[7:0] <= word_send_data[15:8];
-    if (byte_count == 2) send_data[7:0] <= word_send_data[23:16];
-    if (byte_count == 3) send_data[7:0] <= word_send_data[31:24];
+    word_received <= (byte_count == 4);
+    //if (byte_count == 4) send_data[7:0] <= word_send_data[7:0];
+    //if (byte_count == 1) send_data[7:0] <= word_send_data[15:8];
+    //if (byte_count == 2) send_data[7:0] <= word_send_data[23:16];
+    //if (byte_count == 3) send_data[7:0] <= word_send_data[31:24];
   end
 endmodule
