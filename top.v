7c51d995043ee2c9238ef8ada24fe28b8dd6ff73
Steve Kelly <kd2cca@gmail.com>
2020-08-26 22:12:43 -0400

    some adjustments for algo validation

--- top.v
+++ top.v
@@ -67,10 +67,14 @@ module top (
   reg [7:0] message_header;
   always @(posedge word_received) begin
     LED <= !LED;
-    word_send_data[63:0] <= word_data_received[63:0]; // Debug Echo
+
+    // Header Processing
     if (!awaiting_more_words) begin
-      message_header = word_data_received[63:56];
+
+      message_header = word_data_received[63:56]; // Header is 8 MSB
+
       case (message_header)
+
         // 0x01 - Coordinated Move
         // Header: 24 bits for direction
         // Word 1: Increment (signed)
@@ -80,11 +84,13 @@ module top (
           awaiting_more_words <= 1;
           dir <= word_data_received[0];
         end
+
         // 0x03 - Clock divisor (24 bit)
         3: begin
           clock_divisor[23:0] <= word_data_received[23:0];
           awaiting_more_words <= 0;
         end
+
         // 0x04 - Set Microstepping
         4: begin
           // TODO needs to be power of two
@@ -92,13 +98,19 @@ module top (
           awaiting_more_words <= 0;
         end
       endcase
+
+    // Addition Word Processing
     end else begin
       message_word_count = message_word_count + 1;
       case (message_header)
+        // Move Routine
         1: begin
           // the first non-header word is the move duration
           case (message_word_count)
-            1: move_duration[63:0] = word_data_received[63:0];
+            1: begin
+              move_duration[63:0] = word_data_received[63:0];
+              word_send_data[63:0] = last_steps_taken[63:0]; // Prep to send steps
+            end
             2: increment[63:0] = word_data_received[63:0];
             3: begin
                 incrementincrement[63:0] = word_data_received[63:0];
@@ -123,34 +135,38 @@ module top (
   reg steplast = 0;
 
   reg [63:0] move_duration = 64'h0000000004fffff;
-  reg [23:0] clock_divisor = 32;  // should be 32 for 500 khz with bresenham
+  reg [23:0] clock_divisor = 40;  // should be 40 for 400 khz at 16Mhz Clk
 
   reg [63:0] tickaccum = 0;  // move accumulator (clock cycles)
   reg [23:0] clkaccum = 0;  // intra-tick accumulator
 
-  reg signed [63:0] substeps = 64'h8000000000000064; // typemin(Int32) - 100 for buffer
+  reg signed [63:0] substep_accumulator = 64'h7fffffffffffff9b; // typemax(Int64) - 100 for buffer
   reg [63:0] steps_taken = 0;
+  reg [63:0] last_steps_taken = 0;
   reg signed [63:0] increment_r = 0;
   reg signed [63:0] increment = 100000000000;
   reg signed [63:0] incrementincrement = 1000000000;
 
   always @(posedge CLK) begin
-    if ((stepping ^ steplast) && tickaccum <= move_duration) begin
+    if ((stepping ^ steplast) && tickaccum < move_duration) begin
         clkaccum = clkaccum + 1;
-        if (tickaccum == 0) increment_r = increment;
         if (clkaccum[23:0] >= clock_divisor[23:0]) begin
-            clkaccum = 0;
-            tickaccum = tickaccum + 1;
-            increment_r = increment_r + incrementincrement;
-            substeps = substeps + increment_r;
+
+            increment_r = (tickaccum == 0) ? increment : increment_r + incrementincrement;
+            substep_accumulator = substep_accumulator + increment_r;
             // TODO need to set residency on the signal
-            if (substeps >= 0) begin
+            if (substep_accumulator > 0) begin
                 step = 1;
                 steps_taken = steps_taken + 1;
-                substeps = substeps + 64'h8000000000000000;
+                last_steps_taken = steps_taken;
+                substep_accumulator = substep_accumulator - 64'h7fffffffffffff9b;
             end else begin
                  step = 0;
             end
+
+            // Increment tick accumulators
+            clkaccum = 0;
+            tickaccum = tickaccum + 1;
         end
     end else begin
         tickaccum = 0;
