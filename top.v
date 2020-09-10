69e8abe8e16e301ebae30076f233a86aa9692174
Steve Kelly <kd2cca@gmail.com>
2020-09-10 17:08:51 -0400

    Initialize configurable move buffer

--- top.v
+++ top.v
@@ -65,6 +65,7 @@ module top (
                 .dir (dir),
                 .microsteps (microsteps));
 
+
   //
   // Encoder
   //
@@ -88,6 +89,8 @@ module top (
   reg awaiting_more_words = 0;
   reg [7:0] message_word_count = 0;
   reg [7:0] message_header;
+  reg [`MOVE_BUFFER_BITS:0] writemoveind = 0;
+
   always @(posedge word_received) begin
     LED <= !LED;
 
@@ -110,9 +113,10 @@ module top (
           // TODO get direction bits here
           awaiting_more_words <= 1;
 
-          dir <= word_data_received[0];
+          dir_r[writemoveind] <= word_data_received[0];
+
           // Next we send prior ticks
-          word_send_data[63:0] <= tickaccum_last[63:0]; // Prep to send steps
+          //word_send_data[63:0] <= tickdowncount_last[63:0]; // Prep to send steps
         end
 
         // 0x03 - Clock divisor (24 bit)
@@ -146,19 +150,23 @@ module top (
           // the first non-header word is the move duration
           case (message_word_count)
             1: begin
-              move_duration[63:0] = word_data_received[63:0];
-              word_send_data[63:0] = last_steps_taken[63:0]; // Prep to send steps
+              move_duration[writemoveind][63:0] = word_data_received[63:0];
+              //word_send_data[63:0] = last_steps_taken[63:0]; // Prep to send steps
             end
             2: begin
-              increment[63:0] = word_data_received[63:0];
+              increment[writemoveind][63:0] = word_data_received[63:0];
               word_send_data[63:0] = encoder_count_last[63:0]; // Prep to send encoder read
             end
             3: begin
-                incrementincrement[63:0] = word_data_received[63:0];
+                incrementincrement[writemoveind][63:0] = word_data_received[63:0];
                 message_word_count = 0;
                 awaiting_more_words = 0;
-                stepping = ~stepping;
+                stepready[writemoveind] = ~stepready[writemoveind];
+                writemoveind = writemoveind + 1'b1;
                 PIN_22 = ~PIN_22;
+                `ifdef FORMAL
+                  assert(writemoveind <= `MOVE_BUFFER_SIZE);
+                `endif
             end
           endcase
         end
@@ -174,54 +182,67 @@ module top (
   //
 
   // coordinated move execution
+
+  reg [`MOVE_BUFFER_BITS:0] moveind = 0; // Move index cursor
+
   // Latching mechanism for engaging the move. This is currently unbuffered, so TODO
-  reg stepping = 1;
-  reg steplast = 0;
+  reg stepready [`MOVE_BUFFER_SIZE:0];
+  reg stepfinished [`MOVE_BUFFER_SIZE:0];
 
-  reg [63:0] move_duration = 64'h0000000004fffff;
+  reg [63:0] move_duration [`MOVE_BUFFER_SIZE:0];
   reg [23:0] clock_divisor = 40;  // should be 40 for 400 khz at 16Mhz Clk
+  reg dir_r [`MOVE_BUFFER_SIZE:0];
 
-  reg [63:0] tickaccum = 0;  // move accumulator (clock cycles)
-  reg [63:0] tickaccum_last = 0;  // move accumulator (clock cycles)
+  reg [63:0] tickdowncount;  // move down count (clock cycles)
   reg [23:0] clkaccum = 0;  // intra-tick accumulator
 
   reg signed [63:0] substep_accumulator = 0; // typemax(Int64) - 100 for buffer
-  reg [63:0] steps_taken = 0;
-  reg [63:0] last_steps_taken = 0;
   reg signed [63:0] increment_r;
-  reg signed [63:0] increment = 100000000000;
-  reg signed [63:0] incrementincrement = 1000000000;
+  reg signed [63:0] increment [`MOVE_BUFFER_SIZE:0];
+  reg signed [63:0] incrementincrement [`MOVE_BUFFER_SIZE:0];
 
-  wire PIN_21 = step;
+  reg finishedmove = 1; // flag inidicating a move has been finished, so load next
 
   always @(posedge CLK) begin
-    if ((stepping ^ steplast) && tickaccum < move_duration) begin
-        clkaccum = clkaccum + 1;
-        if (clkaccum[23:0] == clock_divisor[23:0]) begin
 
-            increment_r = (tickaccum == 0) ? increment : increment_r + incrementincrement;
-            substep_accumulator = substep_accumulator + increment_r;
-            // TODO need to set residency on the signal
-            if (substep_accumulator > 0) begin
-                step = 1;
-                steps_taken = steps_taken + 1;
-                last_steps_taken = steps_taken;
-                substep_accumulator = substep_accumulator - 64'h7fffffffffffff9b;
-            end else begin
-                 step = 0;
-            end
+    // Load up the move duration
+    if (finishedmove & (stepfinished[moveind] ^ stepready[moveind])) begin
+      tickdowncount = move_duration[moveind];
+      finishedmove = 0;
+    end
 
-            // Increment tick accumulators
-            clkaccum = 0;
-            tickaccum = tickaccum + 1;
-            tickaccum_last = tickaccum;
-            encoder_count_last = encoder_count;
+    // check if this move has been done before
+    if(!finishedmove & (stepfinished[moveind] ^ stepready[moveind])) begin
+
+      // DDA clock divisor
+      clkaccum = clkaccum + 1;
+      if (clkaccum[23:0] == clock_divisor[23:0]) begin
+        dir = dir_r[moveind]; // set direction
+        // TODO For N axes
+        increment_r = (tickdowncount == move_duration[moveind]) ? increment[moveind] : increment_r + incrementincrement[moveind];
+        substep_accumulator = substep_accumulator + increment_r;
+        // TODO need to set residency on the signal
+        if (substep_accumulator > 0) begin
+          step = 1;
+          substep_accumulator = substep_accumulator - 64'h7fffffffffffff9b;
+        end else begin
+          step = 0;
         end
-    end else begin
-        tickaccum = 0;
-        steplast = stepping;
-        steps_taken = 0;
+
+        // Increment tick accumulators
         clkaccum = 0;
+        tickdowncount = tickdowncount - 1'b1;
+        encoder_count_last = encoder_count;
+        // See if we finished the segment and incrment the buffer
+        if(tickdowncount == 0) begin
+          stepfinished[moveind] = stepready[moveind];
+          moveind = moveind + 1'b1;
+          finishedmove = 1;
+          `ifdef FORMAL
+            assert(moveind <= `MOVE_BUFFER_SIZE);
+          `endif
+        end
+      end
     end
   end
 endmodule
