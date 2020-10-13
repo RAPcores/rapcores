837715c887e76f3f3759f88a1fff388587aa7475
Steve Kelly <kd2cca@gmail.com>
2020-10-13 16:43:46 -0400

    [statemachine] several pipeline improvements and cleanup encoder read/send

--- src/top.v
+++ src/top.v
@@ -90,7 +90,7 @@ module top (
   // Encoder
   //
   reg signed [63:0] encoder_count;
-  reg signed [63:0] encoder_count_last;
+  reg signed [63:0] encoder_store; // Snapshot for SPI comms
   reg [7:0] encoder_multiplier = 1;
   wire encoder_fault;
   quad_enc encoder0 (
@@ -106,20 +106,18 @@ module top (
   // State Machine for handling SPI Messages
   //
 
-  reg awaiting_more_words = 0;
   reg [7:0] message_word_count = 0;
   reg [7:0] message_header;
   reg [`MOVE_BUFFER_BITS:0] writemoveind = 0;
 
+  // check if the Header indicated multi-word transfer
+  wire awaiting_more_words = (message_header == `CMD_COORDINATED_STEP) |
+                             (message_header == `CMD_API_VERSION);
+
   always @(posedge word_received) begin
-    LED <= !LED;
 
-    // Zero out the next word
-    //word_send_data = 0;
-    word_send_data[63:0] <= encoder_count[63:0]; // Prep to send encoder read
-
-    // TODO try this non blocking
-    message_word_count <= message_word_count + 1;
+    // Zero out send data register
+    word_send_data <= 64'b0;
 
     // Header Processing
     if (!awaiting_more_words) begin
@@ -127,26 +125,20 @@ module top (
       // Save CMD header incase multi word transaction
       message_header <= word_data_received[63:56]; // Header is 8 MSB
 
-      // By default assume single word command
-      awaiting_more_words <= 0;
-
       // First word so message count zero
       message_word_count <= 1;
 
       case (word_data_received[63:56])
 
         // Coordinated Move
-        // Header: 24 bits for direction
-        // Word 1: Increment (signed)
-        // Word 2: Increment Increment (signed)
         `CMD_COORDINATED_STEP: begin
-          // TODO get direction bits here
-          awaiting_more_words <= 1;
 
+          // Get Direction Bits
           dir_r[writemoveind] <= word_data_received[0];
 
-          // Next we send prior ticks
-          //word_send_data[63:0] <= tickdowncount_last[63:0]; // Prep to send steps
+          // Store encoder values across all axes Now
+          encoder_store <= encoder_count;
+
         end
 
         // Motor Enable/disable
@@ -170,12 +162,13 @@ module top (
           word_send_data[7:0] <= `VERSION_PATCH;
           word_send_data[15:8] <= `VERSION_MINOR;
           word_send_data[23:16] <= `VERSION_MAJOR;
-          awaiting_more_words <= 1;
         end
       endcase
 
     // Addition Word Processing
     end else begin
+
+      message_word_count <= message_word_count + 1;
 
       case (message_header)
         // Move Routine
@@ -188,24 +181,20 @@ module top (
             end
             2: begin
               increment[writemoveind][63:0] <= word_data_received[63:0];
-              word_send_data[63:0] <= encoder_count_last[63:0]; // Prep to send encoder read
+              word_send_data[63:0] <= encoder_store[63:0]; // Prep to send encoder read
             end
             3: begin
                 incrementincrement[writemoveind][63:0] <= word_data_received[63:0];
                 message_word_count <= 0;
-                awaiting_more_words <= 0;
                 stepready[writemoveind] <= ~stepready[writemoveind];
                 writemoveind <= writemoveind + 1'b1;
+                message_header <= 8'b0; // Reset Message Header
                 `ifdef FORMAL
                   assert(writemoveind <= `MOVE_BUFFER_SIZE);
                 `endif
             end
           endcase
         end
-
-        // Otherwise we did a single word reply and are now done
-        default: awaiting_more_words <= 0;
-
       endcase
     end
   end
@@ -269,10 +258,9 @@ module top (
         // Increment tick accumulators
         clkaccum <= clock_divisor;
         tickdowncount <= tickdowncount - 1'b1;
-        encoder_count_last <= encoder_count;
         // See if we finished the segment and incrment the buffer
         if(tickdowncount == 0) begin
-          stepfinished[moveind] <= stepready[moveind];
+          stepfinished[moveind] <= ~stepfinished[moveind];
           moveind <= moveind + 1'b1;
           finishedmove <= 1;
           `ifdef FORMAL
