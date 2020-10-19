6b75542b9488c6bf36450ec74fdc28a886cea73c
Steve Kelly <kd2cca@gmail.com>
2020-10-19 00:12:48 -0400

    init shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+
+# nix.shell: RAPCore Development Environment
+
+with import <nixpkgs> {};
+
+let
+
+  # These are all the packages that will be available inside the nix-shell
+  # environment.
+  buildInputs = with pkgs;
+    # these are generally useful packages for tests, verification, synthesis
+    # and deployment, etc
+    [ yosys symbiyosys nextpnr icestorm trellis
+      z3 boolector yices
+    ];
+
+# Export a usable shell environment
+in runCommand "rapcore-shell" { inherit buildInputs; } ""
