f2985e4f307e1e02a8989ecba2ff73970e0d42fd
Tonokip <tonokip@gmail.com>
2020-10-27 13:17:58 -0500

    add rapbo pin map

---
+++ boards/rapbo/rapbo.v
@@ -0,0 +1,18 @@
+
+// Enable SPI Interface
+`define SPI_INTERFACE
+
+// Enable Buffer DTR pin
+//`define BUFFER_DTR
+
+// Enable Move Done Pin
+//`define MOVE_DONE
+
+// Motor Definitions
+`define DUAL_HBRIDGE 1
+
+// Encoder Count
+`define QUAD_ENC 1
+
+// Change the Move Buffer Size. Should be power of two
+//`define MOVE_BUFFER_SIZE 4
