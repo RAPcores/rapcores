89b2fac7302e554bc74fafbf970c2dc607ac1462
Steve Kelly <kd2cca@gmail.com>
2020-11-16 16:05:49 -0500

    [nix] pin nixpkgs to 20.09

--- shell.nix
+++ shell.nix
@@ -1,7 +1,15 @@
 
 # nix.shell: RAPCore Development Environment
 
-with import <nixpkgs> {};
+# Pin the nixpkgs to stable
+with import (builtins.fetchTarball {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-unstable-2020-09";
+  # Commit hash
+  url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
+}) {};
 
 let
 
