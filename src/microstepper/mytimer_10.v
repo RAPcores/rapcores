c0b2d61be5d073e216c75234c0088df75858795f
johnnyr <johnny@ultimachine.com>
2020-12-01 01:41:35 -0600

    preset params for mytimers

---
+++ src/microstepper/mytimer_10.v
@@ -0,0 +1,29 @@
+module mytimer_10 (
+    clk,
+    resetn,
+    start_enable,
+    start_time,
+    timer
+);
+  parameter WIDTH = 10;
+
+  input clk;
+  input resetn;
+  input start_enable;
+  input [WIDTH-1:0] start_time;
+  output [WIDTH-1:0] timer;
+
+  reg [WIDTH-1:0] counter;
+
+  assign timer = counter;
+
+  always @(posedge clk) begin
+  if (!resetn)
+    counter <= 0;
+  else if( start_enable )
+    counter <= start_time;
+  else if( counter > 0 )
+    counter <= counter - 1'b1;
+end
+
+endmodule
