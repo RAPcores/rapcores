9dd0124246c4e57d9797f5b012ba1ef5f6f4f415
johnnyr <johnny@ultimachine.com>
2020-12-04 21:11:12 -0600

    testbench with Jason and Steve round 3

--- testbench/coil.v
+++ testbench/coil.v
@@ -7,10 +7,14 @@ module coil (
     input           s_l1,
     input           s_h1,
     output  [12:0]   current,
+    output positive,
+    output negative,
 );
     reg     [12:0]   current;
 
-    wire on = (s_l0 && s_h1) | (s_l1 && s_h0);
+    wire on = (!s_l0 && !s_h1) | (!s_l1 && !s_h0);
+    wire positive = (!s_l0 && !s_h1);
+    wire negative = (!s_l1 && !s_h0);
 
     always @(posedge clk) begin
         if (!resetn) begin
