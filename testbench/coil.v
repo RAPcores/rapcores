cf4ace60e71e523c0a570444c418fe24e58612c5
johnnyr <johnny@ultimachine.com>
2020-12-04 19:10:59 -0600

    testbench debugging

--- testbench/coil.v
+++ testbench/coil.v
@@ -2,16 +2,15 @@
 module coil (
     input           clk,
     input           resetn,
+    input           s_l0,
+    input           s_h0,
     input           s_l1,
     input           s_h1,
-    input           s_l2,
-    input           s_h2,
     output  [12:0]   current,
 );
     reg     [12:0]   current;
-    reg     [7:0]   cnt;
 
-    wire on = (s_l1 && s_h2) | (s_l1 && s_h2);
+    wire on = (s_l0 && s_h1) | (s_l1 && s_h0);
 
     always @(posedge clk) begin
         if (!resetn) begin
