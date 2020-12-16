ac995b5bb4751ad5cdd999c8118942e94ff35996
Steve Kelly <kd2cca@gmail.com>
2020-12-16 00:39:27 -0500

    cleanups

--- testbench/spi_tb.v
+++ testbench/spi_tb.v
@@ -34,7 +34,8 @@ module testbench(
   // SCK can't be faster than every two clocks ~ use 4
   reg [1:0] SCK_r = 0;
   wire SCK;
-  assign SCK = (SCK_r == 2'b11 || SCK_r == 2'b10);
+  assign SCK = (SCK_r == 2'b11 || SCK_r == 2'b10); // even out the wave
+
   reg initialized = 0;
   always @(posedge clk) begin
     SCK_r <= SCK_r + 1'b1;
@@ -48,8 +49,6 @@ module testbench(
   // Locals
   reg [63:0] word_data_received;
   reg [63:0] word_send_data;
-
-  wire COPI;
 
   // TB data
   reg [63:0] word_data_tb;
@@ -66,10 +65,6 @@ module testbench(
                 .word_received(word_received),
                 .word_data_received(word_data_received));
 
-  //reg [7:0] tx_byte;
-
-  reg [3:0] bit_count;
-
   initial begin
     word_send_data = 64'h00000000005fffff;
     word_data_tb = 64'hbeefdeaddeadbeef;
@@ -77,19 +72,17 @@ module testbench(
   end
 
   reg [3:0] bit_count = 4'b0;
-  assign COPI = tx_byte[7];
-  reg started = 0;
+  wire COPI = tx_byte[7]; //MSB mode 0
 
+  // shift out the bits
   always @(posedge COPI_tx) begin
-    //if (started) begin
-      tx_byte = {tx_byte[6:0], 1'b0};
-      bit_count = bit_count + 1'b1;
-      if (bit_count == 4'b1000) begin
-        word_data_tb = {8'b0, word_data_tb[63:8]};
-        tx_byte = word_data_tb[7:0];
-        bit_count = 4'b0;
-      end
-    //end else started = 1;
+    tx_byte = {tx_byte[6:0], 1'b0};
+    bit_count = bit_count + 1'b1;
+    if (bit_count == 4'b1000) begin
+      word_data_tb = {8'b0, word_data_tb[63:8]};
+      tx_byte = word_data_tb[7:0];
+      bit_count = 4'b0;
+    end
   end
 
 endmodule
