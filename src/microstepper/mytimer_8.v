a2b90785cfeb9cf5578e5947ea0b1a33e2af67f3
Tonokip <tonokip@gmail.com>
2020-12-17 01:29:09 -0600

    fix most or all verilator warnings for 'done' not being used in cell

--- src/microstepper/mytimer_8.v
+++ src/microstepper/mytimer_8.v
@@ -4,20 +4,21 @@ module mytimer_8 (
     input               resetn,
     input               start_enable,
     input  [WIDTH-1:0]  start_time,
-    output [WIDTH-1:0]  timer,
-    output        reg   done // single cycle timer done event
+    output [WIDTH-1:0]  timer
+    //output        reg   done // single cycle timer done event
 );
   parameter WIDTH = 8;
-
-  initial done = 0;
 
   reg [WIDTH-1:0] counter;
   reg run = 1;
   assign timer = counter;
+  reg done;
 
   always @(posedge clk) begin
-  if (!resetn)
+  if (!resetn) begin
     counter <= 0;
+    done <= 0;
+  end
   else if( start_enable ) begin
     counter <= start_time;
     run <= 1;
