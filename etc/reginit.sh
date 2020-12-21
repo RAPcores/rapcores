d2d672fceccb578efceab7eed408f96b6094262e
Steve Kelly <kd2cca@gmail.com>
2020-12-20 21:31:42 -0500

    add additional search for initializers to reginit.sh

--- etc/reginit.sh
+++ etc/reginit.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 
+# Reg initialization - disallowed
+
 grep -nrP '(reg)(.*)([=<])(.*)(;)' src/*
+
+grep -nrP '(.*)([=<])(.*)(;)' src/* | grep -v 'assign' | grep -v 'wire' | grep -v '<='
