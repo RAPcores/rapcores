23fc7e90d82a97a5167c09c20e58316db5e40454
Steve Kelly <kd2cca@gmail.com>
2020-12-20 21:49:33 -0500

    several initialization fixes on resetn

--- src/spi_state_machine.v
+++ src/spi_state_machine.v
@@ -277,6 +277,17 @@ module spi_state_machine(
     message_word_count <= 0;
     message_header <= 0;
 
+    word_received_r <= 2'b0;
+
+    // TODO change to for loops
+    move_duration[0] <= 64'b0;
+    move_duration[1] <= 64'b0;
+    increment[0] <= 64'b0;
+    increment[1] <= 64'b0;
+    incrementincrement[0] <= 64'b0;
+    incrementincrement[1] <= 64'b0;
+    encoder_store <= 64'b0;
+
   end else if (resetn) begin
     word_received_r <= {word_received_r[0], word_received};
     if (word_received_r == 2'b01) begin
