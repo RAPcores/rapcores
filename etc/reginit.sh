205f7a9e5d10633347f6200bcd5a633908c39775
Steve Kelly <kd2cca@gmail.com>
2021-01-05 16:54:36 -0500

    update reginit script with counts

--- etc/reginit.sh
+++ etc/reginit.sh
@@ -1,7 +1,19 @@
 #! /usr/bin/env bash
 
+# Synchronous assigns 
+syncs="$(grep -nrP '(.*)([=<])(.*)(;)' src/* | grep -v 'assign' | grep -v 'wire' | grep -v 'parameter' | grep -v '==' | grep -v '<=')"
+printf "Synchronous assigns:\n"
+echo "$syncs"
+
 # Reg initialization - disallowed
+regs="$(grep -nrP '(reg)(.*)([=<])(.*)(;)' src/*)"
+printf "\nRegister initializations:\n"
+echo "$regs"
 
-grep -nrP '(reg)(.*)([=<])(.*)(;)' src/*
+# Stats
+synccount=`echo "$syncs" | wc -l`
+count=`echo "$regs" | wc -l`
 
-grep -nrP '(.*)([=<])(.*)(;)' src/* | grep -v 'assign' | grep -v 'wire' | grep -v '<='
+printf "\n"
+echo Synchronous count: $synccount
+echo Register initialization count: $count
