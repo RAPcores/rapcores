e40049a3a67117ab9e93df32570162a5dccbd5ee
Steve Kelly <kd2cca@gmail.com>
2021-01-19 15:04:53 -0500

    [dda] make clock divider a square wave and timer should trigger on rising edge

--- src/clock_divider.v
+++ src/clock_divider.v
@@ -1,7 +1,7 @@
 `default_nettype none
 
 /*
-Generate a divided clock that continually runs.
+Generate a divided clock (square wave)
 */
 module clock_divider #(parameter divider_bits = 8)
 (
@@ -12,15 +12,20 @@ module clock_divider #(parameter divider_bits = 8)
 );
 
   reg [divider_bits-1:0] accum;
+  reg tick_r;
 
-  assign tick = (accum == divider);
+  assign tick = tick_r;
 
   always @(posedge clk) begin
     if (!resetn) begin
       accum <= 0;
+      tick_r <= 0;
     end else begin
       accum <= accum + 1'b1;
-      if (accum == divider) accum <= 0;
+      if (accum == (divider>>1)) begin
+        accum <= 0;
+        tick_r <= ~tick_r;
+      end
     end
   end
 
