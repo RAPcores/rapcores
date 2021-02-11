483b14ad1381623a4f5f143f1f3eccf9cc842216
Steve Kelly <kd2cca@gmail.com>
2021-02-11 18:09:01 -0500

    [dual h] rework phase polarity in prep for vector model

--- src/dual_hbridge.v
+++ src/dual_hbridge.v
@@ -49,26 +49,19 @@ module dual_hbridge #(
     wire brake_b = brake;
   end
 
-  assign phase_a1 = (enable) ? phase_table[phase_ct][0] : brake_a;
-  assign phase_a2 = (enable) ? phase_table[phase_ct][1] : brake_a;
-  assign phase_b1 = (enable) ? phase_table[phase_ct][2] : brake_b;
-  assign phase_b2 = (enable) ? phase_table[phase_ct][3] : brake_b;
+  // determine phase polarity from quadrant
+  wire [3:0] phase_polarity;
+  assign phase_polarity = (phase_ct < 2) ? 4'b1010 : (phase_ct < 4) ? 4'b0110 : (phase_ct < 6) ? 4'b0101 : 4'b1001;
+
+  assign phase_a1 = (enable) ? phase_polarity[0] : brake_a;
+  assign phase_a2 = (enable) ? phase_polarity[1] : brake_a;
+  assign phase_b1 = (enable) ? phase_polarity[2] : brake_b;
+  assign phase_b2 = (enable) ? phase_polarity[3] : brake_b;
 
   assign abs_increment = 3'b100 >> microsteps;
   assign phase_inc = dir ? abs_increment : -abs_increment; // Generate increment, multiple of microsteps\
 
   reg [1:0] step_r;
-
-  always @(negedge resetn) begin
-    phase_table[0] <= 4'b1010;
-    phase_table[1] <= 4'b0010;
-    phase_table[2] <= 4'b0110;
-    phase_table[3] <= 4'b0100;
-    phase_table[4] <= 4'b0101;
-    phase_table[5] <= 4'b0001;
-    phase_table[6] <= 4'b1001;
-    phase_table[7] <= 4'b1000;
-  end
 
   always @(posedge step) begin
       // TODO: Need to add safety SPI or here
