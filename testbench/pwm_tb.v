917eb611476486129222be7edbf0604dfca49674
Steve Kelly <kd2cca@gmail.com>
2021-02-12 14:44:41 -0500

    [tb] add simple pwm tb

---
+++ testbench/pwm_tb.v
@@ -0,0 +1,14 @@
+
+`include "../src/pwm.v"
+
+module pwm_tb(input  wire clk,
+              input  wire resetn,
+              output wire pwm);
+
+  pwm #(.bits(8)) va (.clk(clk),
+          .resetn (resetn),
+          .val(8'd7),
+          .pwm(pwm));
+
+
+endmodule
