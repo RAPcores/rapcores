9864e6a067fddd0d343976ca3f0cfd3ac214b3f8
Steve Kelly <kd2cca@gmail.com>
2021-02-27 19:34:36 -0500

    [tachi] init

---
+++ boards/tachi/tachi.v
@@ -0,0 +1,31 @@
+
+// Enable SPI Interface
+`define SPI_INTERFACE
+
+// Enable Buffer DTR pin
+//`define BUFFER_DTR
+
+// Enable Move Done Pin
+//`define MOVE_DONE
+// TODO MOVE_DONE BROKEN ON MULTIAXIS
+
+// Enable Halt Input
+//`define HALT
+
+// Motor Definitions
+`define DUAL_HBRIDGE 4
+`define VREF_AB
+
+`define MOTOR_COUNT 4
+
+// Encoder Count
+`define QUAD_ENC 1
+
+// Use a PLL for PWM generation
+`define PWMPLL
+
+
+`define ENCODER_BITS 24
+
+// Change the Move Buffer Size. Should be power of two
+//`define MOVE_BUFFER_SIZE 4
