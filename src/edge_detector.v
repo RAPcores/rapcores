2f24b38231c2b1feef9421272e8ebd68dfdc0481
Steve Kelly <kd2cca@gmail.com>
2021-03-01 11:52:45 -0500

    [spi] return to triple buffered edge detector fixes #172

--- src/edge_detector.v
+++ src/edge_detector.v
@@ -28,3 +28,33 @@ module falling_edge_detector (
 
   assign out = in_q && !in;
 endmodule
+
+
+
+module rising_edge_detector_tribuf ( 
+  input clk,
+  input in,
+  output out
+);
+  reg [2:0] in_q;
+
+  always @(posedge clk) begin
+    in_q <= {in_q[1:0], in};
+  end
+
+  assign out = (in_q == 3'b001);
+endmodule
+
+module falling_edge_detector_tribuf ( 
+  input clk,
+  input in,
+  output out
+);
+  reg [2:0] in_q;
+
+  always @(posedge clk) begin
+    in_q <= in;
+  end
+
+  assign out = (in_q == 3'b100);
+endmodule
