07008045d73162e54d730f1511fd14f33df8ea47
Steve Kelly <kd2cca@gmail.com>
2021-03-09 16:34:31 -0500

    [svm] make center aligned PWM optional and explain

--- src/space_vector_modulator.v
+++ src/space_vector_modulator.v
@@ -11,7 +11,8 @@
 module space_vector_modulator #(
    parameter current_bits = 4,
    parameter microstep_bits = 8, // should not be greater than 8
-   parameter phase_ct_bits = 8
+   parameter phase_ct_bits = 8,
+   parameter center_aligned = 1
 ) (
     input clk,
     input resetn,
@@ -23,7 +24,7 @@ module space_vector_modulator #(
 );
 
 
-  // Table of phases (BRAM on FPGA)
+  // Table of phase agnles (BRAM on FPGA)
   reg [7:0] phase_table [0:255];
 
   // Load sine table into BRAM
@@ -37,23 +38,44 @@ module space_vector_modulator #(
   wire [microstep_bits+current_bits-1:0] pwm_a = phase_a[7:(8-microstep_bits)]*current[7:(8-current_bits)];
   wire [microstep_bits+current_bits-1:0] pwm_b = phase_b[7:(8-microstep_bits)]*current[7:(8-current_bits)];
 
-  // Determine delay for center aligned PWM
-  wire [microstep_bits+current_bits-2:0] pwm_delay_a = (pwm_a >= pwm_b) ? 0 : (pwm_b-pwm_a)>>1;
-  wire [microstep_bits+current_bits-2:0] pwm_delay_b = (pwm_b >= pwm_a) ? 0 : (pwm_a-pwm_b)>>1;
+  // PWM Types:
+  //
+  // Center Aligned
+  // A: ___|-------|____
+  // B: ______|-|_______
+  //
+  // Normal
+  // A: ___|-------|____
+  // B: ___|-|__________
 
-  // Microstep*current -> vector angle voltage reference
-  // Center aligned for better response characteristics
-  pwm_delayed #(.bits(microstep_bits+current_bits)) ma (.clk(pwm_clk),
-          .resetn (resetn),
-          .val(pwm_a),
-          .delay(pwm_delay_a),
-          .pwm(vref_a));
-  pwm_delayed #(.bits(microstep_bits+current_bits)) mb (.clk(pwm_clk),
-          .resetn (resetn),
-          .val(pwm_b),
-          .delay(pwm_delay_b),
-          .pwm(vref_b));
+  if (center_aligned) begin
+    // Determine delay for center aligned PWM
+    wire [microstep_bits+current_bits-2:0] pwm_delay_a = (pwm_a >= pwm_b) ? 0 : (pwm_b-pwm_a)>>1;
+    wire [microstep_bits+current_bits-2:0] pwm_delay_b = (pwm_b >= pwm_a) ? 0 : (pwm_a-pwm_b)>>1;
 
+    // Microstep*current -> vector angle voltage reference
+    // Center aligned for better response characteristics
+    pwm_delayed #(.bits(microstep_bits+current_bits)) ma (.clk(pwm_clk),
+            .resetn (resetn),
+            .val(pwm_a),
+            .delay(pwm_delay_a),
+            .pwm(vref_a));
+    pwm_delayed #(.bits(microstep_bits+current_bits)) mb (.clk(pwm_clk),
+            .resetn (resetn),
+            .val(pwm_b),
+            .delay(pwm_delay_b),
+            .pwm(vref_b));
+  end else begin
+    // Microstep*current -> vector angle voltage reference
+    pwm #(.bits(microstep_bits+current_bits)) ma (.clk(pwm_clk),
+            .resetn (resetn),
+            .val(pwm_a),
+            .pwm(vref_a));
+    pwm #(.bits(microstep_bits+current_bits)) mb (.clk(pwm_clk),
+            .resetn (resetn),
+            .val(pwm_b),
+            .pwm(vref_b));
+  end
 
   always @(posedge clk) begin
     if (resetn) begin
