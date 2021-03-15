8b52aee51cd94b430e67085ea3c242603f5856db
Steve Kelly <kd2cca@gmail.com>
2021-03-15 19:59:09 -0400

    [enc] remove index code for now

--- src/quad_enc.v
+++ src/quad_enc.v
@@ -8,12 +8,8 @@ module quad_enc #(
   input wire  clk,
   input wire  a,
   input wire  b,
-  input wire  i,
   output reg faultn,
-  output reg signed [encbits-1:0] count,
-  output wire step_a,
-  output wire step_b,
-  output wire step_i
+  output reg signed [encbits-1:0] count
   //input [7:0] multiplier
   );
 
@@ -21,9 +17,8 @@ module quad_enc #(
 
   reg [2:0] a_stable, b_stable, i_stable;  //Hold sample before compare for stability
 
-  assign step_a = a_stable[1] ^ a_stable[2];  //Step if a changed
-  assign step_b = b_stable[1] ^ b_stable[2];  //Step if b changed
-  assign step_i = i_stable[1] ^ i_stable[2];  //Step if b changed
+  wire step_a = a_stable[1] ^ a_stable[2];  //Step if a changed
+  wire step_b = b_stable[1] ^ b_stable[2];  //Step if b changed
   wire step = step_a ^ step_b;  //Step if a xor b stepped
   wire direction = a_stable[1] ^ b_stable[2];  //Direction determined by comparing current sample to last
   wire signed increment = (direction) ? 1 : -1;
@@ -38,7 +33,6 @@ module quad_enc #(
     else begin
       a_stable <= {a_stable[1:0], a};  //Shift new a in. Last 2 samples shift to bits 2 and 1
       b_stable <= {b_stable[1:0], b};  //Shift new b in
-      i_stable <= {i_stable[1:0], i};
 
       if (step_a & step_b)  //We do not know direction if both inputs triggered on single clock
         faultn <= 0;
