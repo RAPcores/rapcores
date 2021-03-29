aa14e0803d25a930ef13b8109306166fc19b6eb2
Steve Kelly <kd2cca@gmail.com>
2021-03-29 18:53:31 -0400

    init upduino3

---
+++ boards/upduino3/upduino3.mk
@@ -0,0 +1,9 @@
+PIN_DEF = upduino3.pcf
+ARCH = ice40
+DEVICE = up5k
+PACKAGE = sg48
+FREQ = 12
+PROGRAMMER = iceprog -d i:0x0403:0x6014
+SPIFREQ = 64
+PWMFREQ = 140
+SYNTH_FLAGS = -abc9 -device u -dff
