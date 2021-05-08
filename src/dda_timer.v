28a8299a6286a2c2bcc9da5e4154993235decc93
Steve Kelly <kd2cca@gmail.com>
2021-05-08 14:11:49 -0400

    [dda] use more efficient rollover and sign change

--- src/dda_timer.v
+++ src/dda_timer.v
@@ -13,21 +13,24 @@ module dda_timer #(parameter dda_bits = 64)
   input CLK
 );
 
-  reg signed [dda_bits-1:0] substep_accumulator; // typemax(Int64) - 100 for buffer
+  reg [dda_bits-1:0] substep_accumulator; // typemax(Int64) - 100 for buffer
   reg signed [dda_bits-1:0] increment_r;
 
-  // Step Trigger condition
-  reg step_r;
+  // Step assumulate condition
   wire dda_tick_rising;
-  assign step = step_r;
-
   rising_edge_detector dda_rising (.clk(CLK), .in(dda_tick), .out(dda_tick_rising));
+
+  // Step trigger when top bit changes
+  wire step_f, step_r;
+  rising_edge_detector step_rising (.clk(CLK), .in(substep_accumulator[dda_bits-1]), .out(step_r));
+  falling_edge_detector step_falling (.clk(CLK), .in(substep_accumulator[dda_bits-1]), .out(step_f));
+
+  assign step = step_r | step_f;
 
   always @(posedge CLK) if (!resetn) begin
 
     substep_accumulator <= {dda_bits{1'b0}}; // typemax(Int64) - 100 for buffer
     increment_r <= {dda_bits{1'b0}};
-    step_r <= 0;
 
   end else if (resetn) begin
 
@@ -37,14 +40,6 @@ module dda_timer #(parameter dda_bits = 64)
     end
 
     if(dda_tick_rising) begin
-
-      // Step taken, rollback accumulator
-      if (substep_accumulator > 0) begin
-        step_r <= 1;
-        substep_accumulator <= substep_accumulator - 64'h7fffffffffffff9b;
-      end else begin
-        step_r <= 0;
-      end
 
       if (executing_move) begin
 
