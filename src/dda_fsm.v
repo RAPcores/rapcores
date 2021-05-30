9ce2d6b0dbc43c218fdaa8c89995afd2d6dd2141
Steve Kelly <kd2cca@gmail.com>
2021-05-30 12:43:48 -0400

    [dda] cleanup registered outputs

--- src/dda_fsm.v
+++ src/dda_fsm.v
@@ -14,7 +14,8 @@ module dda_fsm #(parameter buffer_bits = 2,
   input [move_duration_bits-1:0] move_duration,
   output loading_move,
   output executing_move,
-  output move_done,
+  output reg move_done,
+  output reg finishedmove,
   output [buffer_bits-1:0] moveind,
   input [buffer_size-1:0] stepready,
   output buffer_dtr
@@ -24,30 +25,24 @@ module dda_fsm #(parameter buffer_bits = 2,
   // Buffer latching
   (* onehot *) reg [buffer_size-1:0] stepfinished;
 
-  assign moveind = moveind_r;
-  reg [buffer_bits-1:0] moveind_r;
-
   // State managment
-  wire finishedmove = finishedmove_r;
   wire processing_move = (stepfinished[moveind] ^ stepready[moveind]);
   assign loading_move = finishedmove & processing_move;
   assign executing_move = !finishedmove & processing_move;
 
   reg [move_duration_bits-1:0] tickdowncount;
-  reg move_done_r;
-  reg finishedmove_r;
   reg [1:0] dda_tick_r;
 
   always @(posedge clk) if (!resetn) begin
-    move_done_r <= 0;
-    finishedmove_r <= 1; // set 1 init so we are in 'loading_move'
+    move_done <= 0;
+    finishedmove <= 1; // set 1 init so we are in 'loading_move'
     stepfinished <= 0;
-    moveind_r <= 0;
+    moveind <= 0;
   end else if (resetn) begin
 
     if (loading_move) begin
       tickdowncount <= move_duration;
-      finishedmove_r <= 0;
+      finishedmove <= 0;
     end
 
     // catch rising edge on DDA tick and downcount
@@ -58,15 +53,13 @@ module dda_fsm #(parameter buffer_bits = 2,
 
     // Trigger move finished
     if (tickdowncount == 0 && executing_move) begin
-      finishedmove_r <= 1;
-      move_done_r <= ~move_done_r;
-      moveind_r <= moveind_r + 1'b1;
+      finishedmove <= 1;
+      move_done <= ~move_done;
+      moveind <= moveind + 1'b1;
       stepfinished[moveind] <= ~stepfinished[moveind]; // flip latch to done
     end
   end
 
-  // Buffer flow control outputs
-  assign move_done = move_done_r;
   assign buffer_dtr = ~(~stepfinished == stepready);
 
 
