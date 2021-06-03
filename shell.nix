18a920d5136218651abcee2b73eb5bf74d7a043d
Steve Kelly <kd2cca@gmail.com>
2021-06-02 21:37:22 -0400

    [nix] update stable channel to 21.05

--- shell.nix
+++ shell.nix
@@ -4,11 +4,11 @@
 # Pin the nixpkgs to stable
 with import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "nixos-unstable-2020-09";
+  name = "nixos-2021-05";
   # Commit hash
-  url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
+  sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
 }) {};
 
 let
