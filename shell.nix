1689809ffad5d5560ba8e9835e3797cd73bff397
Steve Kelly <kd2cca@gmail.com>
2021-07-14 11:49:46 -0400

    [nix] merge in Python requirements

--- shell.nix
+++ shell.nix
@@ -2,29 +2,37 @@
 # nix.shell: RAPCore Development Environment
 
 # Pin the nixpkgs to stable
-with import (builtins.fetchTarball {
-  # Descriptive name to make the store path easier to identify
-  name = "nixos-2021-05";
-  # Commit hash
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
-  # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
-}) {};
+with (import ./nix/inputs.nix);
 
 with import (./nix/npm/default.nix) {};
 
 let 
-verible = import ./nix/verible.nix { inherit pkgs; };
+verible = import ./nix/verible.nix { inherit pkgs; }; # TODO Upstream?
 
 in pkgs.mkShell {
-  # These are all the packages that will be available inside the nix-shell
-  # environment.
   buildInputs = with pkgs;
-    # these are generally useful packages for tests, verification, synthesis
-    # and deployment, etc
-    [ yosys verilog verilator svlint symbiyosys nextpnr icestorm trellis
-      yices tinyprog fujprog openocd
+    [ 
+
+      # Frontends
+      yosys verilog verilator
+      
+      # Support
+      svlint verible
+      
+      # Formal
+      symbiyosys yices 
+
+      # Bitstream Generation
+      nextpnr icestorm trellis
+
+      # Programming tools
+      tinyprog fujprog openocd
+
+      # Docs
       netlistsvg
-      verible
+
+      # Python libs
+      mach-nix.mach-nix
+      (import ./nix/python/python.nix)
        ];
 }
