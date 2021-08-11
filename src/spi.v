fb3ce4413d985584ae0c89ec138c6ea7dbd9af50
Steve Kelly <kd2cca@gmail.com>
2021-08-10 20:24:14 -0400

    [spi] some cleanups

--- src/spi.v
+++ src/spi.v
@@ -1,28 +1,36 @@
 // SPDX-License-Identifier: ISC
 `default_nettype none
 
-// Mode 0 8Bit transfer SPI Peripheral implementation
+
+// SPI Bus implementation (Mode 0 only)
+// Parameters:
+//  - word_bits: Number of bits in a word
+//
+// Note: It is assumed the base transaction is an 8 bit byte,
+// and multi-byte transfers are little endian. This module
+// will wait until the requested number of bits have been
+// received, before signaling the transaction is complete.
 module SPI #(
-  parameter word_bits = 64,
-  parameter endianness = "little"
-  )(
-    input            clk,
-    input            resetn,
-    input            SCK,
-    input            CS,
-    input            COPI,
-    output           CIPO,
-    input      [word_bits-1:0] tx_byte,
-    output wire    [word_bits-1:0] rx_byte,
-    output           rx_byte_ready
+  parameter word_bits = 64
+)(
+    input            clk,    // System clock
+    input            resetn, // Reset active low
+    input            SCK,    // SPI clock
+    input            CS,     // Chip select
+    input            COPI,   // Controller out Peripheral in
+    output           CIPO,   // Controller in Peripheral out
+    input [word_bits-1:0]  tx_byte, // Transmit data
+    output [word_bits-1:0] rx_byte, // Receive data
+    output           rx_byte_ready // Receive data ready
 );
 
   localparam counter_bits = $clog2(word_bits);
   localparam byte_count = word_bits/8;
 
   // Registers to sync IO with FPGA clock
-  reg [1:0] COPIr;
-  reg [1:0] CSr;
+  reg COPIr;
+  reg CSr;
+  reg [1:0] SCKr;
 
   // shift register for recieved bits, endian correction is pipelined
   reg [word_bits-1:0] rx_shreg;
@@ -35,18 +43,12 @@ module SPI #(
   reg [counter_bits-1:0] rxbitcnt; // counts up
   reg [counter_bits-1:0] txbitcnt; // counts down
 
-  // Assign wires for SPI events, registers assigned in block below
-  wire SCK_risingedge;
-  wire SCK_fallingedge;
-  // TODO a normal edge detector should work here with a single stage shift reg
-  rising_edge_detector_tribuf sck_rising (.clk(clk), .in(SCK), .out(SCK_risingedge));
-  falling_edge_detector_tribuf sck_falling (.clk(clk), .in(SCK), .out(SCK_fallingedge));
-
-  wire CS_active = ~CSr[1];  // active low
-  wire COPI_data = COPIr[1];
   // CIPO pin (tristated per convention)
-  assign CIPO = (CS_active) ? tx_byte[txbitcnt] : 1'bZ;
+  assign CIPO = (~CSr) ? tx_byte[txbitcnt] : 1'bZ;
 
+  // TODO generate this
+  // Endianness correction
+  // We do this here to avoid having to do it in the shift register
   if (word_bits == 64) begin
     assign rx_byte = {rx_shreg[0:7],
                       rx_shreg[8:15],
@@ -56,17 +58,24 @@ module SPI #(
                       rx_shreg[40:47],
                       rx_shreg[48:55],
                       rx_shreg[56:63]};
-  end if (word_bits == 32) begin
+  end else if (word_bits == 32) begin
     assign rx_byte = {rx_shreg[0:7],
                       rx_shreg[8:15],
                       rx_shreg[16:23],
                       rx_shreg[24:31]};
+  end else if (word_bits == 16) begin
+    assign rx_byte = {rx_shreg[0:7],
+                      rx_shreg[8:15]};
+  end else if (word_bits == 8) begin
+    assign rx_byte = rx_shreg[0:7];
+  end else begin
+    $error("SPI: Unsupported word width");
   end
 
   always @(posedge clk) begin
     if (!resetn) begin
       // Registers to sync IO with FPGA clock
-      COPIr <= 2'b0;
+      COPIr <= 1'b0;
 
       // Output Byte and ready flag
       rx_byte_ready_r <= 0;
@@ -76,18 +85,17 @@ module SPI #(
       rxbitcnt <= {counter_bits{1'b0}}; // counts up
       txbitcnt <= {counter_bits{1'b1}}; // counts down
     end else if (resetn) begin
+      COPIr <= COPI;
+      CSr <= CS;
+      SCKr <= {SCKr[0],SCK};
 
-      // Use a 2 bit shift register to sync COPI with FPGA clock
-      COPIr <= {COPIr[0], COPI};
-      CSr <= {CSr[0], CS};
-
-      if (CS_active) begin
+      if (~CSr) begin
         // Recieve increment on rising edge
-        if (SCK_risingedge) begin
+        if (SCKr == 2'b01) begin
           rxbitcnt <= rxbitcnt + 1'b1;
           // Shift in Recieved bits ( we pipeline endianness above)
-          rx_shreg <= {rx_shreg[word_bits-2:0], COPI_data};
-        end else if (SCK_fallingedge) begin
+          rx_shreg <= {rx_shreg[word_bits-2:0], COPIr};
+        end else if (SCKr == 2'b10) begin
           txbitcnt <= txbitcnt - 1'b1; // rolls over
           // Trigger Byte recieved
           rx_byte_ready_r <= (txbitcnt == {counter_bits{1'b0}});
@@ -105,62 +113,3 @@ module SPI #(
   end
 
 endmodule
-
-
-//
-//// 32 bit word SPI wrapper for Little endian 8 bit transfers
-////
-//module SPIWord #(parameter bits = 64) (
-//    input wire        clk,
-//    input wire        resetn,
-//    input wire        SCK,
-//    input wire        CS,
-//    input wire        COPI,
-//    output wire       CIPO,
-//    input wire [bits-1:0] word_send_data,
-//    output wire       word_received,
-//    output reg [bits-1:0] word_data_received
-//);
-//
-//  // SPI Initialization
-//  // The standard unit of transfer is 8 bits, MSB
-//  wire rx_byte_ready;  // high when a byte has been received
-//  wire [7:0] rx_byte;
-//  wire [7:0] tx_byte;
-//
-//  SPI spi0 (.clk(clk),
-//            .resetn (resetn),
-//            .CS(CS),
-//            .SCK(SCK),
-//            .CIPO(CIPO),
-//            .COPI(COPI),
-//            .tx_byte(tx_byte),
-//            .rx_byte(rx_byte),
-//            .rx_byte_ready(rx_byte_ready));
-//
-//  reg [$clog2(bits/8)-1:0] byte_count;
-//  wire rx_byte_ready_rising;
-//  reg word_received_r;
-//
-//  rising_edge_detector ready_rising (.clk(clk), .in(rx_byte_ready), .out(rx_byte_ready_rising));
-//
-//  // Recieve Shift Register
-//  always @(posedge clk) if (!resetn) begin
-//    word_data_received <= {bits{1'b0}};
-//    byte_count <= 0;
-//    word_received_r <= 0;
-//  end else if (resetn) begin
-//    if (rx_byte_ready_rising) begin
-//      byte_count <= byte_count + 1'b1;
-//      word_data_received <= {rx_byte[7:0], word_data_received[bits-1:8]};
-//      if (&byte_count) word_received_r <= 1'b1;
-//      else word_received_r <= 1'b0;
-//    end
-//  end
-//
-//  assign word_received = word_received_r;
-//
-//  assign tx_byte[7:0] = word_send_data[byte_count*8 +: 8];
-//
-//endmodule
-//
